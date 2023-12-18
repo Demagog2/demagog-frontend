41d246577b57fd22caff21b0a75bcff797c43aa2
vaclavbohac <vaclav.bohac@productboard.com>
2023-12-18 20:29:19 +0100

    Display article tag articles

--- pages/tag/[slug].tsx
+++ pages/tag/[slug].tsx
@@ -1,13 +1,184 @@
-import type { NextPage } from 'next'
-// TODO - Tag content, list articles, pagination
+import type { NextPageContext } from 'next'
+import apolloClient from '@/libs/apollo-client'
+import gql from 'graphql-tag'
+import ArticleItem, { ArticleDetailFragment } from '@/components/article/Item'
+import { parsePage } from '@/libs/pagination'
+import ArticleTags, {
+  ArticleTag,
+  ArticleTagsFragment,
+} from '@/components/article/Tags'
 
-const Tag: NextPage = () => {
-    return (
-        <div className='container'>
-            <h1>Detail tag</h1>
-        </div>
-    )
+const PAGE_SIZE = 10
+
+export async function getServerSideProps({ query }: NextPageContext) {
+  const page = parsePage(query?.page)
+
+  const { data } = await apolloClient.query({
+    query: gql`
+      query articleTag($slug: String!, $limit: Int, $offset: Int) {
+        articleTagBySlug(slug: $slug) {
+          title
+          description
+          articles(limit: $limit, offset: $offset) {
+            ...ArticleDetail
+          }
+        }
+        articleTags(limit: 5) {
+          ...ArticleTagDetail
+        }
+      }
+      ${ArticleTagsFragment}
+      ${ArticleDetailFragment}
+    `,
+    variables: {
+      slug: query.slug,
+      limit: PAGE_SIZE,
+      offset: (page - 1) * PAGE_SIZE,
+    },
+  })
+
+  return {
+    props: {
+      articleTag: data.articleTagBySlug,
+      articleTags: data.articleTags,
+      page,
+    },
+  }
 }
 
+type TagProps = {
+  page: number
+  articleTag: {
+    title: string
+    description: string
+    articles: {
+      id: string
+      perex: string
+      slug: string
+      illustration: string
+      title: string
+      speakers: {
+        id: string
+        name: string
+        slug: string
+        avatar: string
+      }[]
+      articleType: string
+      source: {
+        medium: {
+          name: string
+        }
+        releasedAt: string
+      }
+      publishedAt: string
+    }[]
+  }
+  articleTags: ArticleTag[]
+}
+
+const Tag = (props: TagProps) => {
+  const topArticles = props.articleTag.articles.slice(0, 4)
+  const bottomArticles =
+    props.articleTag.articles.length > 4
+      ? props.articleTag.articles.slice(4, props.articleTag.articles.length)
+      : []
+
+  return (
+    <div className="container">
+      <div
+        className="row g-5 g-lg-10 flex-lg-row-reverse"
+        data-controller="components--modal"
+      >
+        <div className="col col-12">
+          <ArticleTags tags={props.articleTags} />
+        </div>
+        <div className="col col-12 col-lg-4">
+          <div className="bg-dark-light text-white p-5 p-lg-8 rounded-l mb-10">
+            <div className="expander expander-dark-light">
+              <div className="w-100 position-relative">
+                <h2 className="display-5 fw-bold mb-4">
+                  {props.articleTag.title}
+                </h2>
+                <div className="fs-6 mb-10 dark-content">
+                  {props.articleTag.description}
+                </div>
+              </div>
+            </div>
+            {/*<div className="d-flex">*/}
+            {/*  <a*/}
+            {/*    href="#"*/}
+            {/*    className="text-white text-underline mt-3"*/}
+            {/*    data-target="components--mobile-expander.link"*/}
+            {/*    data-action="click->components--mobile-expander#toggle"*/}
+            {/*  >*/}
+            {/*    <span className="py-2 fs-5">Zobrazit více</span>*/}
+            {/*  </a>*/}
+            {/*</div>*/}
+          </div>
+        </div>
+        {props.page === 1 && (
+          <div className="col col-12 col-lg-8 mb-0 mb-lg-10">
+            <div className="row row-cols-1 g-5 g-lg-10">
+              {topArticles.map((article) => (
+                <ArticleItem
+                  key={article.id}
+                  article={article}
+                  prefix="/diskuze/"
+                />
+              ))}
+            </div>
+          </div>
+        )}
+        <div className="col col-12">
+          <div className="row row-cols-1 row-cols-lg-2 g-5 g-lg-10">
+            {bottomArticles.map((article) => (
+              <ArticleItem
+                key={article.id}
+                article={article}
+                prefix="/diskuze/"
+                showPortraits={false}
+                smallerGaps={true}
+              />
+            ))}
+          </div>
+        </div>
+        {props.page > 1 && (
+          <div className="col col-12">
+            <div className="row row-cols-1 row-cols-lg-2 g-10">
+              {props.articleTag.articles.map((article) => (
+                <ArticleItem
+                  key={article.id}
+                  article={article}
+                  showPortraits={false}
+                  smallerGaps={true}
+                />
+              ))}
+            </div>
+          </div>
+        )}
+        <div className="col col-12">
+          <div className="mb-10">
+            {/*{path_to_prev_page(articles) && (*/}
+            {/*  <a*/}
+            {/*    href={path_to_prev_page(articles)}*/}
+            {/*    className="btn h-50px fs-6 me-2 mb-2 px-8"*/}
+            {/*  >*/}
+            {/*    <span>Zobrazit předchozí</span>*/}
+            {/*  </a>*/}
+            {/*)}*/}
+            {/*{path_to_next_page(articles) && (*/}
+            {/*  <a*/}
+            {/*    href={path_to_next_page(articles)}*/}
+            {/*    className="btn h-50px fs-6 me-2 mb-2 px-8"*/}
+            {/*  >*/}
+            {/*    <span>Zobrazit další</span>*/}
+            {/*  </a>*/}
+            {/*)}*/}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export default Tag
