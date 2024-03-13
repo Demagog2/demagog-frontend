54bc13e0ccaf3b59b4b1b5b89139ef3679e0293a
vaclavbohac <vaclav.bohac@productboard.com>
2024-03-13 19:45:39 +0100

    Single logo responsivity

--- components/campaign/CampaignHeader.tsx
+++ components/campaign/CampaignHeader.tsx
@@ -1,15 +1,13 @@
 import Link from 'next/link'
 import style from '@/assets/styles/campaign/styles.module.css'
-import DemagogLogo from '@/assets/images/campaign/demagog-logo-sub.svg'
-import DemagogLogoMobile from '@/assets/images/campaign/demagog-logo-mobile.svg'
 
 export function CampaignHeader() {
   return (
     <header>
       <Link href="https://demagog.cz">
-        <DemagogLogo className={style.logoDemagog} alt="Demagog.cz" />
-        <DemagogLogoMobile
-          className={style.logoDemagogMobile}
+        <img
+          src="/images/demagog-logo-sub.svg"
+          className={style.logoDemagog}
           alt="Demagog.cz"
         />
       </Link>
