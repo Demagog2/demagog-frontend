c4a47756795f1744d1e181d323b3a1049102da32
Zuzana Belaskova <z.belaskova@gmail.com>
2024-03-13 18:45:12 +0100

    Demagog logo size change for mobile/desktop

--- components/campaign/CampaignHeader.tsx
+++ components/campaign/CampaignHeader.tsx
@@ -1,12 +1,17 @@
 import Link from 'next/link'
 import style from '@/assets/styles/campaign/styles.module.css'
 import DemagogLogo from '@/assets/images/campaign/demagog-logo-sub.svg'
+import DemagogLogoMobile from '@/assets/images/campaign/demagog-logo-mobile.svg'
 
 export function CampaignHeader() {
   return (
     <header>
       <Link href="https://demagog.cz">
         <DemagogLogo className={style.logoDemagog} alt="Demagog.cz" />
+        <DemagogLogoMobile
+          className={style.logoDemagogMobile}
+          alt="Demagog.cz"
+        />
       </Link>
     </header>
   )
