eaee5e9c440e2f07fed8a970971b4aea9b256107
vaclavbohac <vaclav.bohac@productboard.com>
2024-03-17 16:55:35 +0100

    Use permanent redirect

--- pages/kampan/index.tsx
+++ pages/kampan/index.tsx
@@ -19,13 +19,13 @@ import Modals from '@/components/Modals'
 import { useShareableState } from '@/libs/useShareableState'
 import { useBetween } from 'use-between'
 import Link from 'next/link'
-import { redirect } from 'next/navigation'
+import { permanentRedirect } from 'next/navigation'
 
 const Campaign: NextPageWithLayout = () => {
   // Redirect to the old website
   // We'll launch the campaign with the Ruby on Rails version of it
   if (process.env.SERVE_CAMPAIGN_WEBSITE_FROM_RAILS) {
-    redirect('https://demagog.cz/kampan')
+    permanentRedirect('https://demagog.cz/kampan')
   }
 
   const [page, setPage] = useState(0)
