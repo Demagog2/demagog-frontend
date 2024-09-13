c1f5524bf818739b93609bbe0eee0c0effb709ff
vaclavbohac <vaclav.bohac@productboard.com>
2024-09-13 16:09:21 +0200

    Fold info panels on mobile

---
+++ components/util/Expander.tsx
@@ -0,0 +1,63 @@
+'use client'
+
+import classNames from 'classnames'
+import { PropsWithChildren, useCallback, useEffect, useState } from 'react'
+
+// The component will fold when viewport is narrower or equal to the breakpoint
+const BREAKPOINT = 992
+
+// The height of the expander in the folded (non-expanded) state
+const EXPANDER_FOLDED_HEIGHT = 100
+
+export function Expander(props: PropsWithChildren<{ className: string }>) {
+  const [isFoldingRequired, setFoldingRequired] = useState(
+    window.innerWidth <= BREAKPOINT
+  )
+
+  const [isFolded, setFolded] = useState(true)
+
+  const toggleFolded = useCallback(() => {
+    setFolded(!isFolded)
+  }, [isFolded, setFolded])
+
+  const onWindowResize = useCallback(() => {
+    setFoldingRequired(window.innerWidth <= BREAKPOINT)
+  }, [setFoldingRequired])
+
+  useEffect(() => {
+    window.addEventListener('resize', onWindowResize)
+
+    return () => {
+      window.removeEventListener('resize', onWindowResize)
+    }
+  }, [onWindowResize])
+
+  if (!isFoldingRequired) {
+    return <div className={props.className}>{props.children}</div>
+  }
+
+  return (
+    <div className={props.className}>
+      <div
+        className={classNames('expander expander-dark-light', {
+          'is-hide': isFolded,
+        })}
+        style={isFolded ? { maxHeight: EXPANDER_FOLDED_HEIGHT } : {}}
+      >
+        {props.children}
+      </div>
+
+      <div className="d-flex">
+        <a
+          href="#"
+          className="text-white text-decoration-underline mt-3"
+          onClick={toggleFolded}
+        >
+          <span className="py-2 fs-5">
+            {isFolded ? 'Zobrazit více' : 'Skrýt obsah'}
+          </span>
+        </a>
+      </div>
+    </div>
+  )
+}
