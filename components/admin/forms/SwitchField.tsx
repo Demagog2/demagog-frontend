58c159cdd2898ae647b41fdb41d190aa438e91d6
vaclavbohac <vaclav.bohac@productboard.com>
2024-09-24 13:25:03 +0200

    Enable switches for published and pinned fields

---
+++ components/admin/forms/SwitchField.tsx
@@ -0,0 +1,25 @@
+import React, { PropsWithChildren } from 'react'
+import { Description, Field } from '@headlessui/react'
+import { Label } from '@/components/admin/forms/Label'
+
+export function SwitchField(
+  props: PropsWithChildren<{
+    htmlFor: string
+    label: string
+    description: string
+  }>
+) {
+  return (
+    <Field className="flex items-center justify-between">
+      <span className="flex flex-grow flex-col">
+        <Label htmlFor={props.htmlFor}>{props.label}</Label>
+
+        <Description className="text-sm text-gray-500">
+          {props.description}
+        </Description>
+      </span>
+
+      {props.children}
+    </Field>
+  )
+}
