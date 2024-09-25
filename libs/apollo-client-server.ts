8eb986cb114b83f62bcef153c26a4529844eb049
Vaclav Bohac <bohac.v@gmail.com>
2024-09-25 13:43:26 +0200

    Add article illustration file field (#202)
    * Add article illustration file field
    
    * Fix build

--- libs/apollo-client-server.ts
+++ libs/apollo-client-server.ts
@@ -1,7 +1,6 @@
 import {
   ApolloClient,
   from,
-  HttpLink,
   InMemoryCache,
   MutationOptions,
   OperationVariables,
@@ -10,27 +9,28 @@ import {
 import { cookies } from 'next/headers'
 import possibleTypes from '@/__generated__/possibleTypes.json'
 import type { DefaultContext } from '@apollo/client/core/types'
+import createUploadLink from 'apollo-upload-client/createUploadLink.mjs'
 
 export function createServerSideApolloClient() {
   const cookieStore = cookies()
 
   const ssrMode = typeof window === 'undefined'
 
-  const httpLink = new HttpLink({
-    uri: process.env.NEXT_PUBLIC_GRAPHQL_URL,
-    fetch,
-    credentials: 'include', // Include cookies in requests
-    headers: ssrMode
-      ? {
-          // On the server, pass the incoming request's cookies to the outgoing request
-          cookie: cookieStore.toString() || '',
-        }
-      : undefined,
-  })
-
   return new ApolloClient({
     ssrMode: typeof window === 'undefined',
-    link: from([httpLink]),
+    link: from([
+      createUploadLink({
+        uri: process.env.NEXT_PUBLIC_GRAPHQL_URL,
+        fetch,
+        credentials: 'include',
+        headers: ssrMode
+          ? {
+              // On the server, pass the incoming request's cookies to the outgoing request
+              cookie: cookieStore.toString() || '',
+            }
+          : undefined,
+      }),
+    ]),
     cache: new InMemoryCache({
       possibleTypes: possibleTypes,
     }),
