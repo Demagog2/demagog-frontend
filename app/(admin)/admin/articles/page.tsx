2830e5263526fc9abe592bb4ef29d4e8ffe392cc
vaclavbohac <vaclav.bohac@productboard.com>
2024-10-02 10:58:11 +0200

    Prepare search form

--- app/(admin)/admin/articles/page.tsx
+++ app/(admin)/admin/articles/page.tsx
@@ -26,10 +26,15 @@ export default async function AdminArticles(props: PropsWithSearchParams) {
   const before: string | null = getStringParam(props.searchParams.before)
   const after: string | null = getStringParam(props.searchParams.after)
 
+  {
+    /* TODO: @zuzubel Step 2. - take the query parameter from the search form */
+  }
+  const term: string | null = null
+
   const { data } = await serverQuery({
     query: gql(`
-      query AdminArticles($articleType: ArticleTypeEnum, $after: String, $before: String) {
-        articlesV2(first: 15, after: $after, before: $before, filter: { includeUnpublished: true, articleType: $articleType }) {
+      query AdminArticles($articleType: ArticleTypeEnum, $after: String, $before: String, $term: String) {
+        articlesV2(first: 15, after: $after, before: $before, filter: { includeUnpublished: true, articleType: $articleType, title: $term }) {
           edges {
             node {
               id
@@ -53,6 +58,7 @@ export default async function AdminArticles(props: PropsWithSearchParams) {
       articleType: toArticleTypeEnum(getStringParam(props.searchParams.type)),
       ...(after ? { after } : {}),
       ...(before ? { before } : {}),
+      ...(term ? { term } : {}),
     },
   })
 
@@ -63,51 +69,61 @@ export default async function AdminArticles(props: PropsWithSearchParams) {
 
         <div className="sm:flex">
           <div className="mt-3 sm:ml-4 sm:mt-0">
-            <label htmlFor="mobile-search-candidate" className="sr-only">
-              Hledat článek
-            </label>
-            <label htmlFor="desktop-search-candidate" className="sr-only">
-              Hledat článek
-            </label>
-            <div className="flex rounded-md shadow-sm">
-              <div className="relative flex-grow focus-within:z-10">
-                <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-                  <MagnifyingGlassIcon
-                    aria-hidden="true"
-                    className="h-5 w-5 text-gray-400"
+            {/* TODO: @zuzubel Step 1 make the following div into a form. Make sure it has input field with a name "q" and it doesn't lose value after submit. */}
+
+            {/* Mobile search */}
+            <div className="visible md:invisible">
+              <label htmlFor="mobile-search-candidate" className="sr-only">
+                Hledat článek
+              </label>
+
+              <div className="flex rounded-md shadow-sm">
+                <div className="relative flex-grow focus-within:z-10">
+                  <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                    <MagnifyingGlassIcon
+                      aria-hidden="true"
+                      className="h-5 w-5 text-gray-400"
+                    />
+                  </div>
+                  <input
+                    id="mobile-search-candidate"
+                    name="ranndom-field-name"
+                    type="text"
+                    placeholder="Hledat"
+                    className="block w-full rounded-none rounded-l-md rounded-r-md border-0 py-1.5 pl-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:hidden"
                   />
                 </div>
-                <input
-                  id="mobile-search-candidate"
-                  name="mobile-search-candidate"
-                  type="text"
-                  placeholder="Hledat"
-                  className="block w-full rounded-none rounded-l-md rounded-r-md border-0 py-1.5 pl-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:hidden"
-                />
-                <input
-                  id="desktop-search-candidate"
-                  name="desktop-search-candidate"
-                  type="text"
-                  placeholder="Hledat článek"
-                  className="hidden w-full rounded-none rounded-l-md rounded-r-md border-0 py-1.5 pl-10 text-sm leading-6 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:block"
-                />
               </div>
-              {/* <button
-              type="button"
-              className="relative -ml-px inline-flex items-center gap-x-1.5 rounded-r-md px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-            >
-              <BarsArrowUpIcon
-                aria-hidden="true"
-                className="-ml-0.5 h-5 w-5 text-gray-400"
-              />
-              Sort
-              <ChevronDownIcon
-                aria-hidden="true"
-                className="-mr-1 h-5 w-5 text-gray-400"
-              />
-            </button> */}
             </div>
+
+            {/* TODO: @zuzubel Step 1.5 make also the following div into a form. Make sure it has input field with a name "q" and it doesn't lose value after submit. */}
+
+            {/* Desktop search */}
+            <form className="invisible md:visible">
+              <label htmlFor="desktop-search-candidate" className="sr-only">
+                Hledat článek
+              </label>
+
+              <div className="flex rounded-md shadow-sm">
+                <div className="relative flex-grow focus-within:z-10">
+                  <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                    <MagnifyingGlassIcon
+                      aria-hidden="true"
+                      className="h-5 w-5 text-gray-400"
+                    />
+                  </div>
+                  <input
+                    id="desktop-search-candidate"
+                    name="random-field-name"
+                    type="text"
+                    placeholder="Hledat článek"
+                    className="hidden w-full rounded-none rounded-l-md rounded-r-md border-0 py-1.5 pl-10 text-sm leading-6 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:block"
+                  />
+                </div>
+              </div>
+            </form>
           </div>
+
           <div className="mt-3 sm:ml-4 sm:mt-0 sm:flex-none flex-shrink-0">
             <NewArticleDropdown />
           </div>
