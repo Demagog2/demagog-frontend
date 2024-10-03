031037148edd7858b8a3204580ea89dc4b5f9c18
Zuzana Belaskova <z.belaskova@gmail.com>
2024-10-03 10:23:58 +0200

    Notification for users added in Admin Layout

--- components/admin/AdminClientLayout.tsx
+++ components/admin/AdminClientLayout.tsx
@@ -352,7 +352,29 @@ export default function AdminClientLayout(
             </div>
           </div>
         </div>
-
+        <div className="flex items-center gap-x-6 bg-indigo-600 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
+          <p className="text-sm leading-6 text-white">
+            <strong className="font-semibold">Admin Preview</strong>
+            <svg
+              viewBox="0 0 2 2"
+              aria-hidden="true"
+              className="mx-2 inline h-0.5 w-0.5 fill-current"
+            >
+              <circle r={1} cx={1} cy={1} />
+            </svg>
+            Ukázka nového adminu. Buďte opatrní se změnami, které děláte,
+            projeví se na webu.
+          </p>
+          <div className="flex flex-1 justify-end">
+            <button
+              type="button"
+              className="-m-3 p-3 focus-visible:outline-offset-[-4px]"
+            >
+              <span className="sr-only">Dismiss</span>
+              <XMarkIcon aria-hidden="true" className="h-5 w-5 text-white" />
+            </button>
+          </div>
+        </div>
         <main className="py-10">
           <div className="px-4 sm:px-6 lg:px-8">{props.children}</div>
         </main>
