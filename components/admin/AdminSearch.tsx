31d73a7461f8c4ff34aa9c475b8fccb203e9038b
vaclavbohac <vaclav.bohac@productboard.com>
2024-10-03 12:39:11 +0200

    Prepare component for bonus points

---
+++ components/admin/AdminSearch.tsx
@@ -0,0 +1,11 @@
+type AdminSearchProps = {
+  label: string
+  defaultValue?: string
+}
+
+export function AdminSearch(props: AdminSearchProps) {
+  {
+    /* Your code goes here. */
+  }
+  return null
+}
