81bca667e3c58da5b3ea98d4054daa0a2918f417
vaclavbohac <vaclav.bohac@productboard.com>
2024-10-03 11:41:49 +0200

    Switch admin sources to sources v2 GraphQL API

--- app/(admin)/admin/sources/page.tsx
+++ app/(admin)/admin/sources/page.tsx
@@ -17,40 +17,60 @@ import {
   ASSESSMENT_STATUS_LABELS,
   ASSESSMENT_STATUS_PROOFREADING_NEEDED,
 } from '@/libs/constants/assessment'
+import { PropsWithSearchParams } from '@/libs/params'
 
 export const metadata: Metadata = {
   title: getMetadataTitle('Seznam diskuzí', 'Administrace'),
 }
 
-export default async function AdminSources() {
+export default async function AdminSources(props: PropsWithSearchParams) {
   const mediaUrl = process.env.NEXT_PUBLIC_MEDIA_URL ?? ''
+
+  const before: string | null = null
+  const after: string | null = null
+  const term: string | null = null
 
   const { data } = await serverQuery({
     query: gql(`
-      query AdminSources {
-        sources {
-          id
-          name
-          releasedAt
-          sourceUrl
-          experts {
-            id
-            fullName
-            avatar(size: small)
+      query AdminSources($after: String, $before: String, $term: String) {
+        sourcesV2(first: 15, after: $after, before: $before, filter: { includeOnesWithoutPublishedStatements: true, name: $term }) {
+          edges {
+            node {
+              id
+              name
+              releasedAt
+              sourceUrl
+              experts {
+                id
+                fullName
+                avatar(size: small)
+              }
+              medium {
+                name
+              }
+              mediaPersonalities {
+                name
+              }
+              statementsCountsByEvaluationStatus {
+                evaluationStatus
+                statementsCount
+              }
+            }
           }
-          medium {
-            name
-          }
-          mediaPersonalities {
-            name
-          }
-          statementsCountsByEvaluationStatus {
-            evaluationStatus
-            statementsCount
+          pageInfo {
+            hasPreviousPage
+            hasNextPage
+            endCursor
+            startCursor
           }
         }
       }
     `),
+    variables: {
+      ...(after ? { after } : {}),
+      ...(before ? { before } : {}),
+      ...(term ? { term } : {}),
+    },
   })
 
   return (
@@ -76,7 +96,13 @@ export default async function AdminSources() {
             </tr>
           </thead>
           <tbody>
-            {data.sources.map((source) => {
+            {data.sourcesV2.edges?.map((edge) => {
+              if (!edge?.node) {
+                return null
+              }
+
+              const source = edge.node
+
               const statementsCountsMap: Record<string, number> =
                 source.statementsCountsByEvaluationStatus.reduce(
                   (carry, item) => {
