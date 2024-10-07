5bd32e15819ac40c85681c000b0ffb0fd213b392
Zuzana Belaskova <z.belaskova@gmail.com>
2024-10-07 15:55:35 +0200

    Created donate form

--- components/site/DonateSidebar.tsx
+++ components/site/DonateSidebar.tsx
@@ -1,8 +1,12 @@
 'use client'
 
 import Script from 'next/script'
+import { useState } from 'react'
 
 export default function DonateSidebar() {
+  const [differentAmountOpened, setDifferentAmountOpened] = useState(false)
+  const [amount, setAmount] = useState('')
+
   return (
     <div className="bg-light text-dark p-5 p-lg-8 rounded-l d-none d-lg-flex">
       <div className="w-100">
@@ -12,16 +16,92 @@ export default function DonateSidebar() {
             Fungujeme díky podpoře od&nbsp;čtenářů, jako jste vy.
           </span>
         </div>
-        <div className="mt-4" data-darujme-widget-token="rfq62o07d045bw95">
-          &nbsp;
-        </div>
-        <Script
-          id="donate-script1"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `+function(w, d, s, u, a, b) {w['DarujmeObject'] = u;w[u] = w[u] || function () { (w[u].q = w[u].q || []).push(arguments) };a = d.createElement(s); b = d.getElementsByTagName(s)[0];a.async = 1; a.src = "https:\/\/www.darujme.cz\/assets\/scripts\/widget.js";b.parentNode.insertBefore(a, b);}(window, document, 'script', 'Darujme');Darujme(1, "rfq62o07d045bw95", 'render', "https:\/\/www.darujme.cz\/widget?token=rfq62o07d045bw95", "270px");`,
-          }}
-        />
+        <form action="">
+          <div>
+            <label htmlFor="onetime">Jednorázově</label>
+            <input
+              type="radio"
+              name="frequency"
+              id="onetime"
+              value="onetime"
+              onClick={() => setDifferentAmountOpened(false)}
+            ></input>
+
+            <label htmlFor="monthly">Měsíčně</label>
+            <input
+              type="radio"
+              name="frequency"
+              id="monthly"
+              value="monthly"
+              onClick={() => setDifferentAmountOpened(false)}
+            ></input>
+          </div>
+          <label htmlFor="differentAmount">Přispět v Kč:</label>
+          {differentAmountOpened ? (
+            <>
+              <input
+                type="number"
+                id="differentAmount"
+                onChange={(e) => setAmount(e.target.value)}
+              />
+            </>
+          ) : (
+            <button
+              type="button"
+              onClick={() => setDifferentAmountOpened(true)}
+            >
+              Jiná částka
+            </button>
+          )}
+
+          <div>
+            <label htmlFor="100" className="btn btn-success">
+              100 Kč
+            </label>
+            <input
+              type="radio"
+              className="btn-check"
+              id="100"
+              name="amount"
+              value="100"
+              onClick={() => {
+                setDifferentAmountOpened(false)
+                setAmount('100')
+              }}
+            />
+            <label htmlFor="300" className="btn btn-success">
+              300 Kč
+            </label>
+            <input
+              type="radio"
+              className="btn-check"
+              id="300"
+              name="amount"
+              value="300"
+              onClick={() => {
+                setDifferentAmountOpened(false)
+                setAmount('300')
+              }}
+            />
+            <label htmlFor="1000" className="btn btn-success">
+              1000 Kč
+            </label>
+            <input
+              type="radio"
+              className="btn-check"
+              id="1000"
+              name="amount"
+              value="1000"
+              onClick={() => {
+                setDifferentAmountOpened(false)
+                setAmount('1000')
+              }}
+            />
+          </div>
+          <button type="submit" className="btn btn-success">
+            {amount ? `Darovat ${amount} Kč` : `Darovat`}
+          </button>
+        </form>
       </div>
     </div>
   )
