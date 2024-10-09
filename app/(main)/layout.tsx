a567e3d97ffeb9914fa820aa68b00e29978c2b77
Vaclav Bohac <bohac.v@gmail.com>
2024-10-09 22:13:32 +0200

    Hide navigation on scroll down (#333)

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -5,6 +5,8 @@ import { GoogleTagManager } from '@next/third-parties/google'
 
 import '../../assets/styles/main.scss'
 import { Inter } from 'next/font/google'
+import { gql } from '@/__generated__'
+import { query } from '@/libs/apollo-client'
 
 // Invalidate pages after n seconds
 // See: https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config#revalidate
@@ -15,16 +17,24 @@ const inter = Inter({
   weight: ['400', '600', '800', '900'],
 })
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const { data } = await query({
+    query: gql(`
+      query header {
+        ...Navigation
+      }
+    `),
+  })
+
   return (
     <html lang="cs">
       <GoogleTagManager gtmId="GTM-NDL7SQZ6" />
       <body>
-        <Header />
+        <Header data={data} />
         <main>{children}</main>
         <Footer />
         <Modals />
