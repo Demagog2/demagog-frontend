011a1eae7e6664f298e96153c5fbda2ec27734f8
Zuzana Belaskova <z.belaskova@gmail.com>
2024-10-09 11:44:08 +0200

    Donate form styling

--- components/site/DonateSidebar.tsx
+++ components/site/DonateSidebar.tsx
@@ -16,69 +16,94 @@ export default function DonateSidebar() {
             Fungujeme díky podpoře od&nbsp;čtenářů, jako jste vy.
           </span>
         </div>
-        <form action="">
-          <div>
-            <label htmlFor="onetime">Jednorázově</label>
-            <input
-              type="radio"
-              name="frequency"
-              id="onetime"
-              value="onetime"
-              onClick={() => setCustomAmountOpened(false)}
-            ></input>
-
-            <label htmlFor="monthly">Měsíčně</label>
-            <input
-              type="radio"
-              name="frequency"
-              id="monthly"
-              value="monthly"
-              onClick={() => setCustomAmountOpened(false)}
-            ></input>
-          </div>
-          <label htmlFor="customAmount">Přispět v Kč:</label>
-          {customAmountOpened ? (
-            <>
+        <div className="widget">
+          <form action="" className="donate-form">
+            <div className="widget-periodic">
               <input
-                type="number"
-                id="customAmount"
-                onChange={(e) => setAmount(e.target.value)}
-              />
-            </>
-          ) : (
-            <button type="button" onClick={() => setCustomAmountOpened(true)}>
-              Jiná částka
-            </button>
-          )}
-          <div>
-            {[100, 300, 1000].map((amount) => {
-              return (
-                <>
-                  <label
-                    htmlFor={amount.toString()}
-                    className="btn btn-success"
-                  >
-                    {amount} Kč
+                type="radio"
+                name="frequency"
+                id="onetime"
+                value="onetime"
+                className="btn-check"
+                onClick={() => setCustomAmountOpened(false)}
+              ></input>
+              <label htmlFor="onetime" className="widget-periodic-button">
+                Jednorázově
+              </label>
+
+              <input
+                type="radio"
+                name="frequency"
+                id="monthly"
+                value="monthly"
+                className="btn-check"
+                onClick={() => setCustomAmountOpened(false)}
+              ></input>
+              <label htmlFor="monthly" className="widget-periodic-button">
+                Měsíčně
+              </label>
+            </div>
+            <div className="widget-custom">
+              <div className="widget-custom-row">
+                <div className="widget-custom-title">
+                  <label htmlFor="customAmount">
+                    Přispět v{' '}
+                    <span className="widget-custom-currency">Kč:</span>
                   </label>
+                </div>
+                {!customAmountOpened && (
+                  <button
+                    type="button"
+                    className="widget-custom-open"
+                    onClick={() => setCustomAmountOpened(true)}
+                  >
+                    Jiná částka
+                  </button>
+                )}{' '}
+              </div>
+
+              {customAmountOpened && (
+                <div className="widget-custom-fieldset">
                   <input
-                    type="radio"
-                    className="btn-check"
-                    id={amount.toString()}
-                    name="amount"
-                    value={amount.toString()}
-                    onClick={() => {
-                      setCustomAmountOpened(false)
-                      setAmount(amount.toString())
-                    }}
+                    type="number"
+                    id="customAmount"
+                    className="widget-custom-input"
+                    onChange={(e) => setAmount(e.target.value)}
                   />
-                </>
-              )
-            })}
-          </div>
-          <button type="submit" className="btn btn-success">
-            {amount ? `Darovat ${amount} Kč` : `Darovat`}
-          </button>
-        </form>
+                </div>
+              )}
+            </div>
+
+            <div className="widget-entries">
+              {[100, 300, 1000].map((amount) => {
+                return (
+                  <div className="widget-entries-item">
+                    <input
+                      type="radio"
+                      className="btn-check"
+                      id={amount.toString()}
+                      name="amount"
+                      value={amount.toString()}
+                      onClick={() => {
+                        setCustomAmountOpened(false)
+                        setAmount(amount.toString())
+                      }}
+                    />
+                    <label
+                      htmlFor={amount.toString()}
+                      className="widget-entries-button"
+                    >
+                      {amount} Kč
+                    </label>
+                  </div>
+                )
+              })}
+            </div>
+            <button type="submit" className="donate-submit">
+              {amount ? `Darovat ${amount} Kč` : `Darovat`}
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   )
