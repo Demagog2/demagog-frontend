167ffcf4933d50a83b594c05bc73daf3b3c9f706
Vaclav Bohac <bohac.v@gmail.com>
2024-10-10 13:25:38 +0200

    Add hidden input for article text segments (#343)
    * Add hidden input for article text segments
    
    * fixup! Add hidden input for article text segments

--- components/admin/forms/SubmitButton.tsx
+++ components/admin/forms/SubmitButton.tsx
@@ -1,12 +1,16 @@
 import { PropsWithChildren } from 'react'
 
-export function SubmitButton(props: PropsWithChildren) {
+export function SubmitButton(
+  props: PropsWithChildren<{ isSubmitting?: boolean }>
+) {
+  const defaultMessage = props.isSubmitting ? 'Ukládání...' : 'Uložit'
   return (
     <button
+      disabled={props.isSubmitting}
       className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
       type="submit"
     >
-      {props.children ?? 'Uložit'}
+      {props.children ?? defaultMessage}
     </button>
   )
 }
