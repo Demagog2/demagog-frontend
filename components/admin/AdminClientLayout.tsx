70a9b02f708cabcf0dd2348a46d43b7ad363956b
Zuzana Belaskova <z.belaskova@gmail.com>
2024-10-11 13:38:37 +0200

    Changed object teams to context and renamed folder media-personalities to moderators

--- components/admin/AdminClientLayout.tsx
+++ components/admin/AdminClientLayout.tsx
@@ -69,7 +69,7 @@ const content = [
   { name: 'Workshopy', href: '/admin/workshops', icon: BookOpenIcon },
 ]
 
-const teams = [
+const context = [
   {
     name: 'Lidé',
     href: '/admin/speakers',
@@ -87,7 +87,7 @@ const teams = [
   },
   {
     name: 'Moderátoři',
-    href: '/admin/media-personalities',
+    href: '/admin/moderators',
     icon: MicrophoneIcon,
   },
 ]
@@ -203,22 +203,22 @@ export default function AdminClientLayout(
                       Kontext
                     </div>
                     <ul role="list" className="-mx-2 space-y-1 mt-2">
-                      {teams.map((team) => (
-                        <li key={team.name}>
+                      {context.map((item) => (
+                        <li key={item.name}>
                           <Link
-                            href={team.href}
+                            href={item.href}
                             className={classNames(
-                              pathname?.startsWith(team.href)
+                              pathname?.startsWith(item.href)
                                 ? 'bg-gray-50 text-indigo-600'
                                 : 'text-gray-700 hover:bg-gray-50 hover:text-indigo-600',
                               'group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6'
                             )}
                           >
                             {
-                              <team.icon
+                              <item.icon
                                 aria-hidden="true"
                                 className={classNames(
-                                  pathname?.startsWith(team.href)
+                                  pathname?.startsWith(item.href)
                                     ? 'text-indigo-600'
                                     : 'text-gray-400 group-hover:text-indigo-600',
                                   'h-6 w-6 shrink-0'
@@ -226,7 +226,7 @@ export default function AdminClientLayout(
                               />
                             }
 
-                            {team.name}
+                            {item.name}
                           </Link>
                         </li>
                       ))}
@@ -334,27 +334,27 @@ export default function AdminClientLayout(
                 </div>
 
                 <ul role="list" className="-mx-2 space-y-1 mt-2">
-                  {teams.map((team) => (
-                    <li key={team.name}>
+                  {context.map((item) => (
+                    <li key={item.name}>
                       <a
-                        href={team.href}
+                        href={item.href}
                         className={classNames(
-                          pathname?.startsWith(team.href)
+                          pathname?.startsWith(item.href)
                             ? 'bg-gray-50 text-indigo-600'
                             : 'text-gray-700 hover:bg-gray-50 hover:text-indigo-600',
                           'group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6'
                         )}
                       >
-                        <team.icon
+                        <item.icon
                           aria-hidden="true"
                           className={classNames(
-                            pathname?.startsWith(team.href)
+                            pathname?.startsWith(item.href)
                               ? 'text-indigo-600'
                               : 'text-gray-400 group-hover:text-indigo-600',
                             'h-6 w-6 shrink-0'
                           )}
                         />
-                        {team.name}
+                        {item.name}
                       </a>
                     </li>
                   ))}
