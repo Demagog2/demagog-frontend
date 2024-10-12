a00694592548e355f98ad30637d61de6c84a8a34
vaclavbohac <vaclav.bohac@productboard.com>
2024-10-12 16:02:01 +0200

    fixup! Add content image upload form

--- app/(admin)/admin/images/new/page.tsx
+++ app/(admin)/admin/images/new/page.tsx
@@ -2,9 +2,10 @@ import { Metadata } from 'next'
 import { AdminImageForm } from '@/components/admin/images/AdminImageForm'
 import { createImage } from '@/app/(admin)/admin/images/actions'
 import { AdminPageTitle } from '@/components/admin/layout/AdminPageTitle'
+import { getMetadataTitle } from '@/libs/metadata'
 
 export const metadata: Metadata = {
-  title: 'Nový obrázek',
+  title: getMetadataTitle('Nový obrázek', 'Administrace'),
 }
 
 export default function NewImage() {
