5d6c2c1a9b9620b6d682ed527bd05b8a98fcdf5f
vaclavbohac <vaclav.bohac@productboard.com>
2024-10-17 12:11:07 +0200

    Fix react errors

--- components/admin/forms/Input.tsx
+++ components/admin/forms/Input.tsx
@@ -8,6 +8,8 @@ export const Input = forwardRef<
   HTMLSelectElement,
   ReturnType<UseFormRegister<{}>> & { hasError?: boolean }
 >(function Input(props, ref) {
+  const { hasError, ...htmlProps } = props
+
   return (
     <div className="relative mt-2 rounded-md shadow-sm">
       <HeadlessInput
@@ -15,13 +17,13 @@ export const Input = forwardRef<
         className={classNames(
           'block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6',
           {
-            'text-red-900  ring-red-300  focus:ring-red-500 ': props.hasError,
+            'text-red-900  ring-red-300  focus:ring-red-500 ': hasError,
           }
         )}
-        aria-invalid={props.hasError}
-        {...props}
+        aria-invalid={hasError}
+        {...htmlProps}
       />
-      {props.hasError && (
+      {hasError && (
         <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
           <ExclamationCircleIcon
             aria-hidden="true"
--- components/admin/forms/Textarea.tsx
+++ components/admin/forms/Textarea.tsx
@@ -1,10 +1,14 @@
-import { HTMLProps } from 'react'
+import { forwardRef, HTMLProps } from 'react'
 
-export function Textarea(props: HTMLProps<HTMLTextAreaElement>) {
+export const Textarea = forwardRef<
+  HTMLTextAreaElement,
+  HTMLProps<HTMLTextAreaElement>
+>(function Textarea(props: HTMLProps<HTMLTextAreaElement>, ref) {
   return (
     <textarea
+      ref={ref}
       className="block mt-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
       {...props}
     />
   )
-}
+})
