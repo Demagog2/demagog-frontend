608f899a792c1e6d318d5fd639da018c9183dd88
Zuzana Belaskova <z.belaskova@gmail.com>
2024-11-01 12:17:55 +0100

    Added form for creating new media

--- app/(admin)/beta/admin/media/new/page.tsx
+++ app/(admin)/beta/admin/media/new/page.tsx
@@ -1,3 +1,6 @@
+import { AdminPage } from '@/components/admin/layout/AdminPage'
+import { AdminPageHeader } from '@/components/admin/layout/AdminPageHeader'
+import { AdminPageTitle } from '@/components/admin/layout/AdminPageTitle'
 import { divide } from 'lodash'
 import { Metadata } from 'next'
 
@@ -6,5 +9,16 @@ export const metadata: Metadata = {
 }
 
 export default function NewMedia() {
-  return <div>Nový pořad</div>
+  return (
+    <AdminPage>
+      <AdminPageHeader>
+        <AdminPageTitle title="Přidat nový pořad" />
+      </AdminPageHeader>
+      <form action="">
+        <label htmlFor="new-media-field"> Základní údaje</label>
+        <input id="new-media-field" type="text" />
+        <button type="submit">Uložit</button>
+      </form>
+    </AdminPage>
+  )
 }
