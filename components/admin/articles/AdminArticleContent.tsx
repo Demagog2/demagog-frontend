421f3d0966a5aaeac5f9cd0c380b1ea49e2a086d
Zuzana Belaskova <z.belaskova@gmail.com>
2024-11-20 15:00:51 +0100

    Speaker shown on props, created quote component

--- components/admin/articles/AdminArticleContent.tsx
+++ components/admin/articles/AdminArticleContent.tsx
@@ -4,6 +4,7 @@ import { AdminStatement } from './segments/AdminStatement'
 import React from 'react'
 import { Iframely } from '@/components/site/Iframely'
 import { AdminArticleV2Preview } from './AdminArticlePreview'
+import { AdminArticleQuote } from './AdminArticleQuote'
 
 const AdminArticleContentFragment = gql(`
   fragment AdminArticleContent on Article {
@@ -102,31 +103,7 @@ export function AdminArticleContent(props: {
                 }
 
                 if (node.__typename === 'BlockQuoteNode') {
-                  return (
-                    <figure
-                      key={cursor}
-                      className="mt-10 border-l border-indigo-600 pl-9"
-                    >
-                      <blockquote className="font-semibold text-gray-900">
-                        <p>{node.text}</p>
-                      </blockquote>
-                      <figcaption className="mt-10 flex items-center gap-x-6">
-                        <img
-                          alt=""
-                          src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=1024&h=1024&q=80"
-                          className="size-12 rounded-full bg-gray-50 me-1"
-                        />
-                        <div className="text-sm/6">
-                          <div className="font-semibold text-gray-900">
-                            Judith Black
-                          </div>
-                          <div className="mt-0.5 text-gray-600">
-                            CEO of Workcation
-                          </div>
-                        </div>
-                      </figcaption>
-                    </figure>
-                  )
+                  return <AdminArticleQuote key={cursor} text={node.text} />
                 }
 
                 if (node.__typename === 'ArticleNode' && node.article) {
