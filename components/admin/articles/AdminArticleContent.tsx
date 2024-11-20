2ad00d2b3c7aae3d78fba25b4a7e75c952f250df
Zuzana Belaskova <z.belaskova@gmail.com>
2024-11-20 11:44:21 +0100

    Created redesign page for articles in admin, added speaker design into quotes

--- components/admin/articles/AdminArticleContent.tsx
+++ components/admin/articles/AdminArticleContent.tsx
@@ -110,6 +110,21 @@ export function AdminArticleContent(props: {
                       <blockquote className="font-semibold text-gray-900">
                         <p>{node.text}</p>
                       </blockquote>
+                      <figcaption className="mt-10 flex items-center gap-x-6">
+                        <img
+                          alt=""
+                          src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=1024&h=1024&q=80"
+                          className="size-12 rounded-full bg-gray-50 me-1"
+                        />
+                        <div className="text-sm/6">
+                          <div className="font-semibold text-gray-900">
+                            Judith Black
+                          </div>
+                          <div className="mt-0.5 text-gray-600">
+                            CEO of Workcation
+                          </div>
+                        </div>
+                      </figcaption>
                     </figure>
                   )
                 }
