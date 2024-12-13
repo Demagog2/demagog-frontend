6d08c6f6b67b501f084e2c9f1c1107d3d57e2305
Zuzana Belaskova <z.belaskova@gmail.com>
2024-12-13 16:48:15 +0100

    h2 changed to div with display-2

--- app/(main)/statements/[slug]/redesign/page.tsx
+++ app/(main)/statements/[slug]/redesign/page.tsx
@@ -204,9 +204,9 @@ export default async function Statement(props: { params: { slug: string } }) {
             )}
           </div>
           <div className="assessment-veracity d-flex flex-column flex-md-row align-items-md-center justify-content-md-start mt-6 mt-md-10">
-            <h2 className="me-md-6 flex-shrink-0">
+            <div className="display-2 fw-bold me-md-6 flex-shrink-0">
               Tento výrok byl ověřen jako
-            </h2>
+            </div>
             <div className="d-flex align-items-center mt-2 mt-md-0">
               <AssessmentVeracityIcon
                 assessment={statement.assessment}
@@ -228,100 +228,6 @@ export default async function Statement(props: { params: { slug: string } }) {
 
           <div className="mt-10">
             <h3 className="display-2 fw-bold">Plné odůvodnění</h3>
-            {/* ASSESSMENT EXPLANATION FRAME */}
-            <div className="mt-6 bg-lightgrey radius-22px text-start px-4 px-md-6">
-              {/* First Part */}
-              <div className="row justify-content-center justify-content-md-start g-4 g-md-6">
-                {/* Avatar Section */}
-                <div className="col-3 d-flex flex-column align-items-center mt-4 mb-2 mb-md-3">
-                  <SourceSpeakerAvatar
-                    statement={statement}
-                    isEmbedded={true}
-                  />
-                </div>
-
-                {/* Quote Section */}
-                <div className="col-9 mt-4 mt-md-7 mb-3 mb-md-6">
-                  <blockquote
-                    className="p-4 fs-6 bg-dark text-white rounded-4 mb-2 position-relative min-h-50px"
-                    data-target="statement--detail.blockquote"
-                  >
-                    <span className="popover-arrow arrow-east d-md-block"></span>
-                    <span
-                      className="fs-6 position-relative"
-                      dangerouslySetInnerHTML={{ __html: statement.content }}
-                    />
-                  </blockquote>
-                  <div className="fs-8">
-                    <cite className="fst-normal">
-                      {statement.source.medium?.name},{' '}
-                      <span className="date">
-                        {statement.source?.releasedAt &&
-                          formatDate(statement.source.releasedAt)}
-                      </span>
-                    </cite>
-                    {statement.tags.length > 0 && (
-                      <div className="d-flex align-items-center mt-1">
-                        <svg
-                          className="me-2"
-                          width="19"
-                          height="22"
-                          viewBox="0 0 19 22"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            fillRule="evenodd"
-                            clipRule="evenodd"
-                            d="M10.6211 0.9425L18.6536 8.9675C18.7239 9.03722 18.7797 9.12017 18.8178 9.21157C18.8558 9.30296 18.8754 9.40099 18.8754 9.5C18.8754 9.59901 18.8558 9.69704 18.8178 9.78843C18.7797 9.87983 18.7239 9.96278 18.6536 10.0325L17.1536 11.5325L16.0886 10.4675L17.0636 9.5L9.56359 2H3.87109V3.5H2.37109V2C2.37109 1.60218 2.52913 1.22064 2.81043 0.93934C3.09174 0.658035 3.47327 0.5 3.87109 0.5H9.56359C9.96078 0.501675 10.3411 0.660809 10.6211 0.9425ZM9.87109 21.5C9.77239 21.5006 9.67454 21.4817 9.58316 21.4443C9.49179 21.407 9.40867 21.352 9.33859 21.2825L1.31359 13.25C1.0319 12.97 0.872768 12.5897 0.871094 12.1925V6.5C0.871094 6.10218 1.02913 5.72064 1.31043 5.43934C1.59174 5.15804 1.97327 5 2.37109 5H8.06359C8.46078 5.00167 8.84108 5.16081 9.12109 5.4425L17.1536 13.4675C17.2239 13.5372 17.2797 13.6202 17.3178 13.7116C17.3558 13.803 17.3754 13.901 17.3754 14C17.3754 14.099 17.3558 14.197 17.3178 14.2884C17.2797 14.3798 17.2239 14.4628 17.1536 14.5325L10.4036 21.2825C10.3335 21.352 10.2504 21.407 10.159 21.4443C10.0676 21.4817 9.9698 21.5006 9.87109 21.5ZM2.37109 6.5V12.1925L9.87109 19.6925L15.5636 14L8.06359 6.5H2.37109ZM5.37109 11C6.19952 11 6.87109 10.3284 6.87109 9.5C6.87109 8.67157 6.19952 8 5.37109 8C4.54267 8 3.87109 8.67157 3.87109 9.5C3.87109 10.3284 4.54267 11 5.37109 11Z"
-                            fill="#111827"
-                          />
-                        </svg>
-
-                        {statement.tags.map((tag, i) => (
-                          <div key={tag.id} className="me-2">
-                            {tag.name}
-                            {i !== statement.tags.length - 1 ? ', ' : ''}
-                          </div>
-                        ))}
-                      </div>
-                    )}
-                  </div>
-                </div>
-              </div>
-
-              {/* Second Part */}
-              <div className="row justify-content-center justify-content-md-end g-4 g-md-6">
-                <div className="col-12 col-md-9 align-self-center mt-md-8">
-                  <div className="d-flex align-items-center justify-content-start justify-content-md-start">
-                    <AssessmentVeracityIcon
-                      assessment={statement.assessment}
-                      isRedesign
-                    />
-                    <AssessmentVeracityLabel
-                      assessment={statement.assessment}
-                      isRedesign
-                    />
-                  </div>
-
-                  <div className="mt-3 mb-3">
-                    <p>
-                      Jasný postoj ke společné evropské měně má více stran.
-                      Hnutí ANO i SPD jasně deklarovaly, že euro odmítají,
-                      dlouhodobě se proti němu staví i ODS.
-                    </p>
-                    <div className="col-md-8 mt-4 d-flex justify-content-between align-items-center">
-                      <a href="#" className="fw-bold me-3">
-                        zobrazit celé odůvodnění
-                      </a>
-                      <div>
-                        <PermanentLink href={'/vyrok/23657/redesign'} />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
 
             <StatementItem
               statement={statement}
