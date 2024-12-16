72b2e001b1cab1d13ad86df19cd250c7d4ded8e8
Vaclav Bohac <bohac.v@gmail.com>
2024-12-16 12:27:40 +0100

    Refactor of source speaker avatar styles

--- components/statement/SourceSpeakerAvatar.tsx
+++ components/statement/SourceSpeakerAvatar.tsx
@@ -2,7 +2,6 @@ import { FragmentType, gql, useFragment } from '@/__generated__'
 import { SpeakerLink } from '../speaker/SpeakerLink'
 import Image from 'next/image'
 import { imagePath } from '@/libs/images/path'
-import classNames from 'classnames'
 
 const SourceSpeakerAvatarFragment = gql(`
   fragment SourceSpeakerAvatar on Statement {
@@ -33,20 +32,16 @@ export function SourceSpeakerAvatar(props: {
         speaker={statement.sourceSpeaker?.speaker}
         className="d-block position-relative"
       >
-        <span
-          className={classNames('symbol symbol-square symbol-circle', {
-            'w-60px h-60px w-md-80px h-md-80px': props.isEmbedded,
-          })}
-        >
-          {statement.sourceSpeaker.speaker.avatar && (
-            <Image
-              src={imagePath(statement.sourceSpeaker.speaker.avatar)}
-              alt={statement.sourceSpeaker.fullName}
-              width={85}
-              height={85}
-            />
-          )}
-        </span>
+        {statement.sourceSpeaker.speaker.avatar && (
+          <Image
+            className="source-speaker-avatar"
+            src={imagePath(statement.sourceSpeaker.speaker.avatar)}
+            alt={statement.sourceSpeaker.fullName}
+            width={85}
+            height={85}
+          />
+        )}
+
         {props.isEmbedded && statement.sourceSpeaker.body?.shortName && (
           <div className="symbol-label d-flex align-items-center justify-content-center w-35px h-35px rounded-circle bg-dark">
             <span className="smallest text-white lh-1 text-center p-2">
