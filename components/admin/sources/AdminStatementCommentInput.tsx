971b3f6bbf6e8747acbfac76c77c8949d70ca7b1
Vaclav Bohac <bohac.v@gmail.com>
2024-12-17 17:27:27 +0100

    Add react mentions to the comment input (#792)

--- components/admin/sources/AdminStatementCommentInput.tsx
+++ components/admin/sources/AdminStatementCommentInput.tsx
@@ -1,14 +1,18 @@
 'use client'
 
 import { FragmentType, gql, useFragment } from '@/__generated__'
-import { imagePath } from '@/libs/images/path'
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import { AdminUserAvatar } from '../users/AdminUserAvatar'
+import { Mention, MentionsInput } from 'react-mentions'
 
 const AdminStatementCommentInputFragment = gql(`
   fragment AdminStatementCommentInput on Query {
     currentUser {
       ...AdminUserAvatar
+    }
+    users(limit: 100) {
+      id
+      fullName
     }
   }   
 `)
@@ -20,7 +24,30 @@ export function AdminStatementCommentInput(props: {
 }) {
   const data = useFragment(AdminStatementCommentInputFragment, props.data)
 
-  const [message, setMessage] = useState('')
+  const [message, setMessage] = useState(
+    'Ahoj, @[Zuzana Bělašková](387), jak se mas?'
+  )
+
+  const suggestions = useMemo(() => {
+    return [
+      ...data.users.map((user) => ({
+        id: user.id,
+        display: user.fullName,
+      })),
+      {
+        id: 'experts',
+        display: 'Editoři',
+      },
+      {
+        id: 'proofreaders',
+        display: 'Korektoři',
+      },
+      {
+        id: 'social_media_managers',
+        display: 'Síťaři',
+      },
+    ]
+  }, [data.users])
 
   return (
     <>
@@ -33,37 +60,65 @@ export function AdminStatementCommentInput(props: {
             <label htmlFor="comment" className="sr-only">
               Přidejte komentář
             </label>
-            <textarea
+            <MentionsInput
               id="comment"
-              rows={3}
-              placeholder="Přidejte komentář..."
-              className="block w-full border-none resize-none text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:border-none focus:outline-0 sm:text-sm/6"
-              defaultValue={''}
-              onChange={(evt) => setMessage(evt.target.value)}
-            />
+              className="admin-comment-input"
+              placeholder="Přidejte komentář"
+              rows={4}
+              value={message}
+              onChange={(_, value) => {
+                setMessage(value)
+              }}
+              allowSpaceInQuery
+              style={{
+                suggestions: {
+                  list: {
+                    backgroundColor: 'white',
+                    border: '1px solid rgba(0,0,0,0.15)',
+                    fontSize: 12,
+                    maxHeight: 200,
+                    overflow: 'scroll',
+                  },
+                  item: {
+                    padding: '5px 10px',
+                    '&focused': {
+                      backgroundColor: 'rgb(206, 230, 249)',
+                    },
+                  },
+                },
+              }}
+            >
+              <Mention
+                className="admin-comment-input-mention"
+                // className="block w-full border-none resize-none text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:border-none focus:outline-0 sm:text-sm/6"
+                trigger="@"
+                data={suggestions}
+                appendSpaceOnAdd
+              />
+            </MentionsInput>
           </div>
-          <div className="flex justify-between pt-2">
-            <div className="shrink-0">
-              <button
-                disabled={message.length === 0}
-                className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                onClick={(evt) => {
-                  evt.preventDefault()
+        </div>
+      </div>
+      <div className="flex justify-between pt-2">
+        <div className="text-sm text-gray-500">
+          Tip: Můžeš zmínit kohokoli z týmu — stačí napsat @ a vybrat koho. Také
+          dostane upozornění na tvůj komentář. @Editoři upozorní všechny editory
+          u tohoto výroku.
+        </div>
+        <div className="shrink-0">
+          <button
+            disabled={message.length === 0}
+            className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            onClick={(evt) => {
+              evt.preventDefault()
 
-                  props.onSubmit(message)
+              props.onSubmit(message)
 
-                  setMessage('')
-                }}
-              >
-                Odeslat
-              </button>
-            </div>
-          </div>
-          <div className="text-sm text-gray-500 mt-2">
-            Tip: Můžeš zmínit kohokoli z týmu — stačí napsat @ a vybrat koho.
-            Také dostane upozornění na tvůj komentář. @Editoři upozorní všechny
-            editory u tohoto výroku.
-          </div>
+              setMessage('')
+            }}
+          >
+            Odeslat
+          </button>
         </div>
       </div>
     </>
