2f83c7fc68dde846835ade1aed1110544cfa4da9
vaclavbohac <vaclav.bohac@productboard.com>
2024-12-19 15:40:51 +0100

    Show number of unread notifications]

--- components/admin/notifications/NotificationIcon.tsx
+++ components/admin/notifications/NotificationIcon.tsx
@@ -1,5 +1,6 @@
 import { BellIcon } from '@heroicons/react/24/outline'
 import { FragmentType, gql, useFragment } from '@/__generated__'
+import { pluralize } from '@/libs/pluralize'
 
 const NotificationIconFragment = gql(`
   fragment NotificationIcon on Query {
@@ -18,12 +19,19 @@ export function NotificationIcon(props: {
     <a
       href="/beta/admin/notifications"
       className="-m-2.5 p-2.5 text-gray-400 hover:text-gray-500"
+      title={`Máte ${data.notificationsV2.totalCount} ${pluralize(data.notificationsV2.totalCount, 'nepřečtené zprávy', 'nepřečtené zprávy', 'nepřečtených zpráv')}.`}
     >
       <span className="sr-only">View notifications</span>
-      <div className="relative">
-        <BellIcon aria-hidden="true" className="h-6 w-6" />
+      <div className="flex flex-row">
+        <div className="relative">
+          <BellIcon aria-hidden="true" className="h-6 w-6" />
+          {data.notificationsV2.totalCount > 0 && (
+            <span className="absolute right-0 bottom-0 block size-2 rounded-full bg-red-500 ring-2 ring-white"></span>
+          )}
+        </div>
+
         {data.notificationsV2.totalCount > 0 && (
-          <span className="absolute right-0 bottom-0 block size-2 rounded-full bg-red-500 ring-2 ring-white"></span>
+          <div className="ml-2">{data.notificationsV2.totalCount}</div>
         )}
       </div>
     </a>
