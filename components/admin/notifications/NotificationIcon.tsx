b8c41e6369fd585e1550c7185639f191d42d83be
Vaclav Bohac <bohac.v@gmail.com>
2024-12-19 16:12:35 +0100

    Improve number of unread notifications (#823)

--- components/admin/notifications/NotificationIcon.tsx
+++ components/admin/notifications/NotificationIcon.tsx
@@ -26,13 +26,13 @@ export function NotificationIcon(props: {
         <div className="relative">
           <BellIcon aria-hidden="true" className="h-6 w-6" />
           {data.notificationsV2.totalCount > 0 && (
-            <span className="absolute right-0 bottom-0 block size-2 rounded-full bg-red-500 ring-2 ring-white"></span>
+            <div className="absolute bottom-auto left-auto right-0 top-0 z-10 inline-block -translate-y-1/2 translate-x-2/4 rotate-0 skew-x-0 skew-y-0 scale-x-100 scale-y-100 whitespace-nowrap rounded-full bg-indigo-600 px-2.5 py-1 text-center align-baseline text-xs font-bold leading-none text-white">
+              {data.notificationsV2.totalCount < 100
+                ? data.notificationsV2.totalCount
+                : '99+'}
+            </div>
           )}
         </div>
-
-        {data.notificationsV2.totalCount > 0 && (
-          <div className="ml-2">{data.notificationsV2.totalCount}</div>
-        )}
       </div>
     </a>
   )
