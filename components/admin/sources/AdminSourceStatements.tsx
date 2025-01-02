84b68dfd40e31906bdbd48adcb691b18cd5777ac
Zuzana Belaskova <z.belaskova@gmail.com>
2025-01-02 15:38:03 +0100

    Displays information about statement assessment progress

--- components/admin/sources/AdminSourceStatements.tsx
+++ components/admin/sources/AdminSourceStatements.tsx
@@ -2,7 +2,10 @@ import { FragmentType, gql, useFragment } from '@/__generated__'
 import { AdminSourceStatementStep } from './AdminSourceStatementStep'
 import { VeracityBadge } from '../veracity/VeracityBadge'
 import { AdminUserAvatar } from '../users/AdminUserAvatar'
-import { ASSESSMENT_STATUS_APPROVED } from '@/libs/constants/assessment'
+import {
+  ASSESSMENT_STATUS_APPROVED,
+  ASSESSMENT_STATUS_BEING_EVALUATED,
+} from '@/libs/constants/assessment'
 import { useRouter } from 'next/navigation'
 
 const SourceStatementsFragment = gql(`
@@ -23,6 +26,8 @@ const SourceStatementsFragment = gql(`
       assessment {
         ...VeracityBadge
         evaluationStatus
+        shortExplanationCharactersLength
+        explanationCharactersLength
         evaluator {
           ...AdminUserAvatar
           fullName
@@ -134,6 +139,37 @@ export function AdminSourceStatements(props: {
                         </dd>
                       </div>
                     </dl>
+
+                    {statement.assessment.evaluationStatus ===
+                      ASSESSMENT_STATUS_BEING_EVALUATED && (
+                      <div className="mt-6 text-sm">
+                        {statement.assessment
+                          .shortExplanationCharactersLength === 0 &&
+                        statement.assessment.explanationCharactersLength ===
+                          0 ? (
+                          <p className="font-medium text-gray-500">
+                            Odůvodnění zatím prázdné
+                          </p>
+                        ) : (
+                          <>
+                            {' '}
+                            <p className="font-medium text-gray-500">
+                              Zkrácené odůvodnění:{' '}
+                              {
+                                statement.assessment
+                                  .shortExplanationCharactersLength
+                              }{' '}
+                              znaků
+                            </p>
+                            <p className="text-gray-500">
+                              Celé odůvodnění:{' '}
+                              {statement.assessment.explanationCharactersLength}{' '}
+                              znaků
+                            </p>
+                          </>
+                        )}
+                      </div>
+                    )}
                   </div>
                 </div>
 
