12fbfd6b057260518834c73b1b2421735da6dce2
Zuzana Belaskova <z.belaskova@gmail.com>
2025-01-06 11:48:37 +0100

    Article sources moved from spans to one div

--- components/article/metadata/DebateArticleMetadata.tsx
+++ components/article/metadata/DebateArticleMetadata.tsx
@@ -29,47 +29,37 @@ export function DebateArticleMetadata(props: {
       {article.articleType === 'default' && article.source && (
         <div className={classNames('mb-5 mb-lg-10 mt-8 mt-md-10')}>
           <h2 className='"fs-2 text-uppercase text-primary'>Ověřili jsme</h2>
-          <div className="row g-1 mt-2">
-            <span className="col col-auto fs-5">
-              {article.source.medium?.name}
-            </span>
+          <div className="mt-4 fs-5">
+            {article.source.medium?.name}{' '}
             {article.source?.releasedAt && (
+              <>ze dne {formatDate(article.source.releasedAt)}</>
+            )}
+            {((article.source.mediaPersonalities?.length ?? 0) > 0 ||
+              article.source.sourceUrl) && (
               <>
-                <span className="col col-auto fs-5">ze dne</span>
-                <span className="col col-auto fs-5">
-                  {formatDate(article.source.releasedAt)}
-                </span>
+                {' '}
+                (
+                {(article.source.mediaPersonalities?.length ?? 0) > 0 && (
+                  <>
+                    {(article.source.mediaPersonalities?.length ?? 0) > 1 ? (
+                      <>moderátoři </>
+                    ) : (
+                      <>moderátor </>
+                    )}
+                    {article.source.mediaPersonalities
+                      ?.map((mediaPersonality: any) => mediaPersonality.name)
+                      .join(', ')}
+                    {article.source.sourceUrl && ', '}
+                  </>
+                )}
+                {article.source.sourceUrl && (
+                  <a href={article.source.sourceUrl} className="ext">
+                    záznam
+                  </a>
+                )}
+                )
               </>
             )}
-
-            <div className="col fs-5">
-              {((article.source.mediaPersonalities?.length ?? 0) > 0 ||
-                article.source.sourceUrl) && (
-                <>
-                  (
-                  {(article.source.mediaPersonalities?.length ?? 0) > 0 && (
-                    <>
-                      {(article.source.mediaPersonalities?.length ?? 0) > 1 ? (
-                        <>moderátoři </>
-                      ) : (
-                        <>moderátor </>
-                      )}
-
-                      {article.source.mediaPersonalities
-                        ?.map((mediaPersonality: any) => mediaPersonality.name)
-                        .join(', ')}
-                      {article.source.sourceUrl && ', '}
-                    </>
-                  )}
-                  {article.source.sourceUrl && (
-                    <a href={article.source.sourceUrl} className="ext">
-                      záznam
-                    </a>
-                  )}
-                  )
-                </>
-              )}
-            </div>
           </div>
         </div>
       )}
