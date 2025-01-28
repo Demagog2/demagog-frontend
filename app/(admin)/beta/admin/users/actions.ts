e814beb9c57b7031b37546d6a417506d4bb30f72
vaclavbohac <vaclav.bohac@productboard.com>
2025-01-28 16:19:35 +0100

    Add create user action and user schema

--- app/(admin)/beta/admin/users/actions.ts
+++ app/(admin)/beta/admin/users/actions.ts
@@ -1,6 +1,12 @@
 'use server'
 import { gql } from '@/__generated__'
+import {
+  CreateUserMutation,
+  CreateUserMutationVariables,
+} from '@/__generated__/graphql'
 import { serverMutation } from '@/libs/apollo-client-server'
+import { CreateActionBuilder } from '@/libs/forms/builders/CreateActionBuilder'
+import { userSchema } from '@/libs/users/user-schema'
 import { redirect } from 'next/navigation'
 
 const adminDeleteUserMutation = gql(`
@@ -23,3 +29,37 @@ export async function deleteUser(userId: string) {
     redirect(`/beta/admin/users`)
   }
 }
+
+const adminCreateUserMutation = gql(`
+  mutation CreateUser($userInput: UserInput!) {
+    createUser(userInput: $userInput) {
+      user {
+        id
+      }
+    }
+  }
+`)
+
+export const createUser = new CreateActionBuilder<
+  typeof userSchema,
+  CreateUserMutation,
+  CreateUserMutationVariables,
+  typeof adminCreateUserMutation
+>(userSchema)
+  .withMutation(adminCreateUserMutation, (data) => {
+    const { emailNotifications = false, ...rest } = data
+    return {
+      userInput: {
+        ...rest,
+        emailNotifications,
+      },
+    }
+  })
+  .withRedirectUrl((data) => {
+    if (data?.createUser?.user) {
+      return `/beta/admin/users/${data?.createUser?.user.id}`
+    }
+
+    return null
+  })
+  .build()
