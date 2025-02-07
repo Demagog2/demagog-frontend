d4694c3a543dafb2faabca632741ac1b7d101a75
Zuzana Belaskova <z.belaskova@gmail.com>
2025-02-07 10:10:42 +0100

    Article tag detail page styling

--- app/(admin)/beta/admin/article-tags/[slug]/page.tsx
+++ app/(admin)/beta/admin/article-tags/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { gql } from '@/__generated__'
+import { LinkButton } from '@/components/admin/forms/LinkButton'
 import { AdminPage } from '@/components/admin/layout/AdminPage'
 import { AdminPageContent } from '@/components/admin/layout/AdminPageContent'
 import { AdminPageHeader } from '@/components/admin/layout/AdminPageHeader'
@@ -10,6 +11,7 @@ import {
   CreditCardIcon,
   UserCircleIcon,
 } from '@heroicons/react/24/outline'
+import classNames from 'classnames'
 import { Metadata } from 'next'
 
 export async function generateMetadata(props: {
@@ -69,41 +71,60 @@ export default async function ArticleTagDetail(props: {
           title={articleTag.title ?? ''}
           description="Detail tagu"
         />
+        <LinkButton href="/beta/admin/article-tags">Zpět</LinkButton>
       </AdminPageHeader>
       <AdminPageContent>
         <div className="border-b border-t border-gray-200 bg-white shadow-sm rounded-lg border">
           <div className="px-4 py-6 sm:px-6 lg:p-8">
             <div className="flex flex-col">
-              {articleTag.published ? (
-                <p className="w-fit rounded-md px-2 py-1 text-xs font-medium ring-1 ring-inset bg-green-50 text-green-700 ring-green-600/20">
-                  Veřejný
-                </p>
-              ) : (
-                <p className="w-fit flex-none rounded-md px-2 py-1 text-xs font-medium ring-1 ring-inset bg-red-50 text-red-700 ring-red-600/10">
-                  Neveřejný
-                </p>
-              )}
-              <div className="mt-3">
+              <p
+                className={classNames(
+                  'w-fit rounded-md px-2 py-1 -ml-1 text-xs font-medium ring-1 ring-inset',
+                  {
+                    'bg-green-50 text-green-700 ring-green-600/20':
+                      articleTag.published,
+                    'bg-red-50 text-red-700 ring-red-600/10':
+                      !articleTag.published,
+                  }
+                )}
+              >
+                {articleTag.published ? 'Veřejný' : 'Neveřejný'}
+              </p>
+
+              <div className="mt-4">
                 <p className="font-medium text-gray-900">Popis:</p>
                 <p className="mt-1 text-sm text-gray-500">
                   {articleTag.description}
                 </p>
               </div>
-              <div className="mt-3">
-                <p className="font-medium text-gray-900">Url:</p>
-                <p className="mt-1 text-sm text-gray-500">{`/tag/${articleTag.slug}`}</p>
-              </div>
-              <div className="mt-3">
-                <p className="font-medium text-gray-900">Pozice:</p>
-                <p className="mt-1 text-sm text-gray-500">{articleTag.order}</p>
-              </div>
-              <div className="mt-3">
-                <p className="font-medium text-gray-900">Ikona</p>
-                <p className="mt-1 text-sm text-gray-500">{articleTag.icon}</p>
-              </div>
-              <div className="mt-3">
-                <p className="font-medium text-gray-900">Statistika</p>
-                <p className="mt-1 text-sm text-gray-500">{articleTag.stats}</p>
+              <div className="grid grid-cols-4 gap-2">
+                <div className="flex flex-col">
+                  <div className="mt-3">
+                    <p className="font-medium text-gray-900">Pozice:</p>
+                    <p className="mt-1 text-sm text-gray-500">
+                      {articleTag.order}
+                    </p>
+                  </div>
+                  <div className="mt-3">
+                    <p className="font-medium text-gray-900">Ikona</p>
+                    <p className="mt-1 text-sm text-gray-500">
+                      {articleTag.icon}
+                    </p>
+                  </div>
+                </div>
+                <div className="flex flex-col">
+                  <div className="mt-3">
+                    <p className="font-medium text-gray-900">Url:</p>
+                    <p className="mt-1 text-sm text-gray-500">{`/tag/${articleTag.slug}`}</p>
+                  </div>
+
+                  <div className="mt-3">
+                    <p className="font-medium text-gray-900">Statistika</p>
+                    <p className="mt-1 text-sm text-gray-500">
+                      {articleTag.stats}
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
