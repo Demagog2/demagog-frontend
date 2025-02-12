01a7a320324538897fc58b45c246a0df2daa8870
Zuzana Belaskova <z.belaskova@gmail.com>
2025-02-12 16:03:45 +0100

    Speaker detail page

--- app/(admin)/beta/admin/speakers/page.tsx
+++ app/(admin)/beta/admin/speakers/page.tsx
@@ -14,6 +14,7 @@ import { serverQuery } from '@/libs/apollo-client-server'
 import { buildGraphQLVariables } from '@/libs/pagination'
 import { imagePath } from '@/libs/images/path'
 import AdminSpeakerDelete from '@/components/speaker/AdminSpeakerDeleteDialog'
+import { PencilIcon } from '@heroicons/react/24/outline'
 
 export const metadata: Metadata = {
   title: getMetadataTitle('Lidé', 'Administrace'),
@@ -109,10 +110,21 @@ export default async function Speakers(props: PropsWithSearchParams) {
 
                       <div className="flex-grow mt-6 sm:ml-6 sm:mt-0 lg:ml-8">
                         <div className="flex justify-between">
-                          <h3 className="text-base font-medium text-gray-900">
-                            {edge.node.fullName}
-                          </h3>
-                          <AdminSpeakerDelete speaker={edge.node} />
+                          <a href={`/beta/admin/speakers/${edge.node.id}`}>
+                            <h3 className="text-base font-medium text-gray-900">
+                              {edge.node.fullName}
+                            </h3>
+                          </a>
+
+                          <div className="flex space-x-3">
+                            <a
+                              href={`/beta/admin/speakers/${edge.node.id}/edit`}
+                              title="Upravit"
+                            >
+                              <PencilIcon className="h-6 w-6 text-gray-400 hover:text-indigo-600 cursor-pointer" />
+                            </a>
+                            <AdminSpeakerDelete speaker={edge.node} />
+                          </div>
                         </div>
 
                         <p className="mt-3 text-sm text-gray-500">
