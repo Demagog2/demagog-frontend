dd045b43bc5ecb6cf25798a2efcb95c19a3ca201
Zuzana Belaskova <z.belaskova@gmail.com>
2025-02-20 12:11:57 +0100

    Create accordion section action

--- app/(admin)/beta/admin/accordion-sections/actions.ts
+++ app/(admin)/beta/admin/accordion-sections/actions.ts
@@ -1,8 +1,14 @@
 'use server'
 
 import { gql } from '@/__generated__'
+import { schema } from '@/libs/accordion-section/schema'
 import { serverMutation } from '@/libs/apollo-client-server'
+import { CreateActionBuilder } from '@/libs/forms/builders/CreateActionBuilder'
 import { redirect } from 'next/navigation'
+import {
+  CreateAccordionSectionMutation,
+  CreateAccordionSectionMutationVariables,
+} from '@/__generated__/graphql'
 
 const adminDeleteAccordionSectionMutation = gql(`
   mutation AdminDeleteAccordionSection($input: DeleteAccordionSectionMutationInput!) {
@@ -33,3 +39,42 @@ export async function deleteAccordionSection(accordionSectionId: string) {
     redirect(`/beta/admin/accordion-sections`)
   }
 }
+
+const adminCreateAccordionSectionMutation = gql(`
+  mutation CreateAccordionSection($input: CreateAccordionSectionMutationInput!) {
+    createAccordionSection(input: $input) {
+      ... on CreateAccordionSectionSuccess {
+        accordionSection {
+          id
+        }
+      }
+      ... on CreateAccordionSectionError {
+        message
+      }
+    }
+  }
+`)
+
+export const createAccordionSection = new CreateActionBuilder<
+  typeof schema,
+  CreateAccordionSectionMutation,
+  CreateAccordionSectionMutationVariables,
+  typeof adminCreateAccordionSectionMutation
+>(schema)
+  .withMutation(adminCreateAccordionSectionMutation, (data) => ({
+    input: {
+      ...data,
+      order: data.order ? Number(data.order) : undefined,
+    },
+  }))
+  .withRedirectUrl((data) => {
+    if (
+      data.createAccordionSection?.__typename ===
+      'CreateAccordionSectionSuccess'
+    ) {
+      return `/beta/admin/accordion-sections/${data.createAccordionSection.accordionSection.id}`
+    }
+
+    return null
+  })
+  .build()
