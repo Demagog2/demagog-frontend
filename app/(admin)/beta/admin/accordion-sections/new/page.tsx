dc6ec8e0d5de29984f706dd0664b47052cb6c70f
Zuzana Belaskova <z.belaskova@gmail.com>
2025-02-20 12:13:13 +0100

    Accordion section form

--- app/(admin)/beta/admin/accordion-sections/new/page.tsx
+++ app/(admin)/beta/admin/accordion-sections/new/page.tsx
@@ -1,9 +1,18 @@
 import { Metadata } from 'next'
+import { getMetadataTitle } from '@/libs/metadata'
+import { AdminAccordionSectionForm } from '@/components/admin/accordion-sections/AdminAccordionSectionForm'
+import { createAccordionSection } from '../actions'
 
 export const metadata: Metadata = {
-  title: 'Přidat novou sekci',
+  title: getMetadataTitle('Nová sekce', 'Administrace'),
 }
 
-export default function AddNewSection() {
-  return <div>Přidat novou sekci</div>
+export default async function AddAccordionSectionNew() {
+  return (
+    <AdminAccordionSectionForm
+      action={createAccordionSection}
+      title="Nová sekce"
+      description="Přidání nové sekce o nás"
+    />
+  )
 }
