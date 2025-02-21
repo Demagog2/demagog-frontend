9228a338afa4ad7aeeda7958a2c7e9e1a82a3dcc
Zuzana Belaskova <z.belaskova@gmail.com>
2025-02-21 12:30:32 +0100

    Accordion Item schema

--- libs/accordion-section/schema.ts
+++ libs/accordion-section/schema.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod'
 
-export const schema = z.object({
+export const accordionSectionSchema = z.object({
   title: z
     .string()
     .trim()
@@ -10,4 +10,18 @@ export const schema = z.object({
     z.number().optional()
   ),
   published: z.preprocess((value) => value === 'on', z.boolean()).optional(),
+})
+
+export const accordionItemSchema = z.object({
+  title: z.string().trim().min(1, 'Název musí obsahovat alespoň jeden znak.'),
+  order: z.preprocess(
+    (val) => (typeof val === 'string' ? parseFloat(val) : val),
+    z.number().optional()
+  ),
+  published: z.preprocess((value) => value === 'on', z.boolean()).optional(),
+  content: z.string().trim().optional(),
+  memberListing: z
+    .preprocess((value) => value === 'on', z.boolean())
+    .optional(),
+  accordionSectionId: z.string(),
 })
