c5fc241c0033fa9ff21ce7efe20eb63c8a33d6e8
Vaclav Bohac <bohac.v@gmail.com>
2025-03-08 20:07:57 +0100

    fixup! Demagog TV (forgotten code)

--- components/admin/sources/AdminSourceHeader.tsx
+++ components/admin/sources/AdminSourceHeader.tsx
@@ -93,7 +93,7 @@ export async function AdminSourceHeader(props: {
 
         {isAuthorized(['statements:edit']) && (
           <a
-            href={`/beta/admin/sources/${props.source.id}/statements-video-marks`}
+            href={`/beta/admin/sources/${source.id}/statements-video-marks`}
             className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
           >
             Propojení s videozáznamem
