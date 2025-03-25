e7285e05ec561e08f591f9bc518f2cf9ea463bf7
Zuzana Belaskova <z.belaskova@gmail.com>
2025-03-25 11:19:29 +0100

    Metadata

--- app/(admin)/beta/admin/tags/[slug]/page.tsx
+++ app/(admin)/beta/admin/tags/[slug]/page.tsx
@@ -3,6 +3,31 @@ import { gql } from '@/__generated__'
 import { serverQuery } from '@/libs/apollo-client-server'
 import { notFound } from 'next/navigation'
 import { AdminPage } from '@/components/admin/layout/AdminPage'
+import { getMetadataTitle } from '@/libs/metadata'
+import { Metadata } from 'next'
+
+export async function generateMetadata(props: {
+  params: { slug: string }
+}): Promise<Metadata> {
+  const {
+    data: { tag },
+  } = await serverQuery({
+    query: gql(`
+      query AdminTagDetailMetadata($id: ID!) {
+        tag(id: $id) {
+          name
+        }
+      }
+    `),
+    variables: {
+      id: props.params.slug,
+    },
+  })
+
+  return {
+    title: getMetadataTitle(`Detail tagu: ${tag?.name}`, 'Administrace'),
+  }
+}
 
 const TagQuery = gql(`
   query AdminTag($id: ID!) {
