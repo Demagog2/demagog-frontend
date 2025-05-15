cfc7d778787895e8b1e1b369e8e4dd55879c4490
Zuzana Belaskova <z.belaskova@gmail.com>
2025-05-15 15:18:16 +0200

    Schema translated to czech, added claim appearances

--- components/admin/articles/integrations/AdminEuroClimateForm.tsx
+++ components/admin/articles/integrations/AdminEuroClimateForm.tsx
@@ -10,17 +10,26 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { ErrorMessage } from '@/components/admin/forms/ErrorMessage'
 import {
   topic,
-  distortionTypes,
-  formatTypes,
+  distortionType,
+  formatType,
 } from '@/libs/integrations/Euro-climate-schema'
 import { Multiselect } from '../../forms/Multiselect'
 import { Select } from '../../forms/Select'
-import { AdminFormHeader } from '../../layout/AdminFormHeader'
 import { SubmitButton } from '../../forms/SubmitButton'
+import {
+  ChevronUpIcon,
+  PlusCircleIcon,
+  TrashIcon,
+  ChevronDownIcon,
+} from '@heroicons/react/24/outline'
+import { useFieldArray } from 'react-hook-form'
+import { useState } from 'react'
 
 type FieldValues = z.output<typeof euroclimateFormSchema>
 
 export function AdminEuroClimateForm() {
+  const [isFormVisible, setIsFormVisible] = useState(false)
+
   const {
     register,
     control,
@@ -30,134 +39,230 @@ export function AdminEuroClimateForm() {
     resolver: zodResolver(euroclimateFormSchema),
     defaultValues: {
       topic: undefined,
-      subtopic: [],
+      subtopics: [],
       distortionType: [],
-      appearanceUrl: '',
-      appearanceDate: '',
-      archiveUrl: '',
-      format: undefined,
+      appearances: [
+        {
+          appearanceUrl: '',
+          appearanceDate: '',
+          archiveUrl: '',
+          format: 'other',
+        },
+      ],
     },
+  })
+
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: 'appearances',
   })
 
   const selectedTopic = watch('topic')
 
   return (
-    <div className="w-full border-t border-gray-900/10 mt-6">
-      <div className="flex justify-end mt-6">
-        <SubmitButton />
+    <>
+      <div className="flex justify-start mt-6">
+        <button
+          type="button"
+          className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-indigo-600 focus:outline-none"
+          onClick={() => setIsFormVisible(!isFormVisible)}
+        >
+          {isFormVisible ? (
+            <>
+              <ChevronUpIcon className="h-5 w-5 mr-1" />
+              Skrýt formulář
+            </>
+          ) : (
+            <>
+              <ChevronDownIcon className="h-5 w-5 mr-1" />
+              Zobrazit formulář
+            </>
+          )}
+        </button>
       </div>
-      <AdminFormContent>
-        <div className="col-span-12 gap-y-5">
-          <Fieldset className="space-y-4 w-full border-b border-gray-900/10 pb-8">
-            <Legend className=" text-base font-semibold leading-7 text-gray-900">
-              Detaily článku
-            </Legend>
-            <div className="grid sm:grid-cols-2 gap-4">
-              <Field>
-                <Label htmlFor="topic">Téma</Label>
-                <Controller
-                  control={control}
-                  name="topic"
-                  render={({ field }) => (
-                    <Select
-                      id="topic"
-                      items={Object.entries(topic).map(([id, topicData]) => ({
-                        label: topicData.label,
-                        value: id,
-                      }))}
-                      placeholder="Vyberte téma"
-                      onChange={(item) => field.onChange(item?.value)}
-                      defaultValue={field.value}
-                      canRemoveItem
+
+      {isFormVisible && (
+        <div className="w-full border-t border-gray-900/10 mt-6">
+          <div className="flex justify-end mt-6">
+            <SubmitButton />
+          </div>
+          <AdminFormContent>
+            <div className="col-span-12 gap-y-5">
+              <Fieldset className="space-y-4 w-full border-b border-gray-900/10 pb-8">
+                <Legend className=" text-base font-semibold leading-7 text-gray-900">
+                  Podrobnosti o článku
+                </Legend>
+                <div className="grid sm:grid-cols-2 gap-4">
+                  <Field>
+                    <Label htmlFor="topic">Téma</Label>
+                    <Controller
+                      control={control}
+                      name="topic"
+                      render={({ field }) => (
+                        <Select
+                          id="topic"
+                          items={Object.entries(topic).map(
+                            ([id, topicData]) => ({
+                              label: topicData.label,
+                              value: id,
+                            })
+                          )}
+                          placeholder="Vyberte téma"
+                          onChange={(item) => field.onChange(item?.value)}
+                          defaultValue={field.value}
+                          canRemoveItem
+                        />
+                      )}
                     />
-                  )}
-                />
-                <ErrorMessage message={errors.topic?.message} />
-              </Field>
+                    <ErrorMessage message={errors.topic?.message} />
+                  </Field>
 
-              <Field>
-                <Label htmlFor="subtopic">Podtéma</Label>
+                  <Field>
+                    <Label htmlFor="subtopics">Podtéma</Label>
 
-                <Multiselect
-                  control={control}
-                  items={
-                    selectedTopic
-                      ? topic[selectedTopic as keyof typeof topic].subtopic.map(
-                          (item) => ({
-                            label: item.label,
-                            value: item.id,
-                          })
-                        )
-                      : []
+                    <Multiselect
+                      control={control}
+                      items={
+                        selectedTopic
+                          ? topic[
+                              selectedTopic as keyof typeof topic
+                            ].subtopics.map((item) => ({
+                              label: item.label,
+                              value: item.id,
+                            }))
+                          : []
+                      }
+                      name="subtopics"
+                      placeholder="Vyberte podtémata"
+                    />
+                  </Field>
+                </div>
+              </Fieldset>
+              <Fieldset className="space-y-4 w-full border-b border-gray-900/10 pb-8">
+                <Legend className="mt-8 text-base font-semibold leading-7 text-gray-900">
+                  Podrobnosti o tvrzení
+                </Legend>
+                <Field>
+                  <Label htmlFor="disctortionType">Typ dezinformace</Label>
+                  <Multiselect
+                    control={control}
+                    items={distortionType}
+                    name="distortionType"
+                    placeholder="Vyberte typ dezinformace"
+                  />
+                </Field>
+              </Fieldset>
+
+              {fields.map((field, index) => (
+                <>
+                  <Fieldset
+                    key={field.id}
+                    className="space-y-4 col-span-8 border-b border-gray-900/10 pb-8 px-6"
+                  >
+                    <div className="flex justify-between items-center">
+                      <Legend className="mt-8 text-base font-semibold leading-7 text-gray-900">
+                        Výskyt tvrzení #{index + 1}
+                      </Legend>
+                      {index > 0 && (
+                        <button
+                          type="button"
+                          onClick={() => remove(index)}
+                          className="ml-4 p-2 text-gray-400 hover:text-indigo-600"
+                          title="Odstranit výskyt"
+                        >
+                          <TrashIcon className="h-5 w-5" />
+                        </button>
+                      )}
+                    </div>
+
+                    <Field>
+                      <Label htmlFor={`appearanceUrl-${index}`}>
+                        URL výskytu tvrzení
+                      </Label>
+                      <Input
+                        type="text"
+                        id={`appearanceUrl-${index}`}
+                        placeholder="Zadejte URL"
+                        {...register(`appearances.${index}.appearanceUrl`, {
+                          required: true,
+                        })}
+                      />
+                      <ErrorMessage
+                        message={
+                          errors.appearances?.[index]?.appearanceUrl?.message
+                        }
+                      />
+                    </Field>
+                    <div className="grid sm:grid-cols-2 gap-4">
+                      <Field>
+                        <Label htmlFor={`appearanceDate-${index}`}>
+                          Datum výskytu
+                        </Label>
+                        <Input
+                          type="date"
+                          {...register(`appearances.${index}.appearanceDate`)}
+                        />
+                      </Field>
+                      <Field>
+                        <Label htmlFor={`format-${index}`}>Formát šíření</Label>
+                        <Controller
+                          control={control}
+                          name={`appearances.${index}.format`}
+                          render={({ field }) => (
+                            <Select
+                              id={`format-${index}`}
+                              items={formatType}
+                              placeholder="Vyberte formát"
+                              onChange={(item) => field.onChange(item?.value)}
+                              defaultValue={field.value}
+                              canRemoveItem
+                            />
+                          )}
+                        />
+                        <ErrorMessage
+                          message={errors.appearances?.[index]?.format?.message}
+                        />
+                      </Field>
+                    </div>
+
+                    <Field>
+                      <Label htmlFor={`archiveUrl-${index}`} isOptional>
+                        Archivní URL
+                      </Label>
+                      <Input
+                        type="text"
+                        id={`archiveUrl-${index}`}
+                        placeholder="Zadejte archivní URL"
+                        {...register(`appearances.${index}.archiveUrl`)}
+                      />
+                    </Field>
+                  </Fieldset>
+                </>
+              ))}
+              <div className="flex justify-end mt-4">
+                <button
+                  type="button"
+                  className="inline-flex justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                  onClick={() =>
+                    append({
+                      appearanceUrl: '',
+                      appearanceDate: '',
+                      archiveUrl: '',
+                      format: undefined,
+                    })
                   }
-                  name="subtopic"
-                  placeholder="Vyberte podtémata"
-                />
-              </Field>
+                >
+                  <PlusCircleIcon
+                    aria-hidden="true"
+                    className="-ml-0.5 h-5 w-5"
+                  />
+                  Přidat výskyt
+                </button>
+              </div>
             </div>
-            <Field>
-              <Label htmlFor="disctortionType">Typ dezinformace</Label>
-              <Multiselect
-                control={control}
-                items={distortionTypes}
-                name="distortionType"
-                placeholder="Vyberte typ dezinformace"
-              />
-            </Field>
-          </Fieldset>
-          <Fieldset className="space-y-4 w-full border-b border-gray-900/10 pb-8">
-            <Legend className="mt-8 text-base font-semibold leading-7 text-gray-900">
-              Podrobnosti o tvrzení
-            </Legend>
-            <Field>
-              <Label htmlFor="appearanceUrl">URL výskytu tvrzení</Label>
-              <Input
-                type="text"
-                id="appearanceUrl"
-                placeholder="Zadejte URL"
-                {...register('appearanceUrl', { required: true })}
-              />
-              <ErrorMessage message={errors.appearanceUrl?.message} />
-            </Field>
-            <div className="grid sm:grid-cols-2 gap-4">
-              <Field>
-                <Label htmlFor="appearanceDate">Datum výskytu</Label>
-                <Input type="date" {...register('appearanceDate')} />
-              </Field>
-              <Field>
-                <Label htmlFor="format">Formát</Label>
-                <Controller
-                  control={control}
-                  name="format"
-                  render={({ field }) => (
-                    <Select
-                      id="format"
-                      items={formatTypes}
-                      placeholder="Vyberte formát"
-                      onChange={(item) => field.onChange(item?.value)}
-                      defaultValue={field.value}
-                      canRemoveItem
-                    />
-                  )}
-                />
-                <ErrorMessage message={errors.format?.message} />
-              </Field>
-            </div>
-
-            <Field>
-              <Label htmlFor="archiveUrl" isOptional>
-                Archivní URL
-              </Label>
-              <Input
-                type="text"
-                id="archiveUrl"
-                placeholder="Zadejte archivní URL"
-                {...register('archiveUrl')}
-              />
-            </Field>
-          </Fieldset>
+          </AdminFormContent>
         </div>
-      </AdminFormContent>
-    </div>
+      )}
+    </>
   )
 }
--- libs/integrations/Euro-climate-schema.ts
+++ libs/integrations/Euro-climate-schema.ts
@@ -2,34 +2,98 @@ import { z } from 'zod'
 
 // Define the topics and their subtopics
 export const topic = {
-  'Extreme weather events': {
-    label: 'Extreme weather events',
-    subtopic: [
-      { id: 'subtopic-a-1', label: 'Increasing temperatures' },
-      { id: 'subtopic-a-2', label: 'Heatwaves' },
-      { id: 'subtopic-a-3', label: 'Floods' },
-      { id: 'subtopic-a-4', label: 'Water scarcity' },
-      { id: 'subtopic-a-5', label: 'Other' },
+  'Extrémní výkyvy počasí': {
+    label: 'Extrémní výkyvy počasí',
+    subtopics: [
+      { id: 'subtopic-a-1', label: 'Rostoucí teploty' },
+      { id: 'subtopic-a-2', label: 'Vlny veder' },
+      { id: 'subtopic-a-3', label: 'Povodně' },
+      { id: 'subtopic-a-4', label: 'Nedostatek vody' },
+      { id: 'subtopic-a-5', label: 'Jiné' },
     ],
   },
-  Transport: {
-    label: 'Transport',
-    subtopic: [
-      { id: 'subtopic-b-1', label: 'Eletric cars', value: 'electric-cars' },
-      { id: 'subtopic-b-2', label: 'Other', value: 'other' },
+  Doprava: {
+    label: 'Doprava',
+    subtopics: [
+      {
+        id: 'subtopic-b-1',
+        label: 'Elektrická auta',
+        value: 'Elektrická auta',
+      },
+      { id: 'subtopic-b-2', label: 'Jiné', value: 'Jiné' },
+    ],
+  },
+  'Obnovitelné zdroje': {
+    label: 'Obnovitelné zdroje',
+    subtopics: [
+      { id: 'subtopic-c-1', label: 'Větrná energie' },
+      { id: 'subtopic-c-2', label: 'Solární panely' },
+      { id: 'subtopic-c-3', label: 'Pobřežní větrná energie' },
+      { id: 'subtopic-c-4', label: 'Jiné' },
+    ],
+  },
+  'Konspirační teorie': {
+    label: 'Konspirační teorie',
+    subtopics: [
+      { id: 'subtopic-d-1', label: 'Chemtrails' },
+      { id: 'subtopic-d-2', label: 'Agenda 2030' },
+      { id: 'subtopic-d-3', label: 'Patnáctiminutová města' },
+      { id: 'subtopic-d-4', label: 'HAARP' },
+      { id: 'subtopic-d-5', label: 'Jiné' },
+    ],
+  },
+  'Fosilní paliva': {
+    label: 'Fosilní paliva',
+    subtopics: [
+      { id: 'subtopic-e-1', label: 'Zemní plyn' },
+      { id: 'subtopic-e-2', label: 'Ropa' },
+      { id: 'subtopic-e-3', label: 'Uhlí' },
+      { id: 'subtopic-e-4', label: 'Jiné' },
+    ],
+  },
+  Odpad: {
+    label: 'Odpad',
+    subtopics: [
+      { id: 'subtopic-f-1', label: 'Plasty' },
+      { id: 'subtopic-f-4', label: 'Jiné' },
+    ],
+  },
+  Jiné: {
+    label: 'Jiné',
+    subtopics: [
+      { id: 'subtopic-g-1', label: 'Popírání změny klimatu' },
+      { id: 'subtopic-g-2', label: 'Spotřeba masa' },
+      { id: 'subtopic-g-3', label: 'Jiné' },
     ],
   },
 }
 
-export const distortionTypes = [
-  { label: 'Fabricated information', value: 'fabricated' },
-  { label: 'Distortion type B', value: 'distortion-b' },
+export const distortionType = [
+  { label: 'Neprokázané', value: 'Nepotvrzene' },
+  { label: 'Satira považovaná za pravdivou', value: 'distortion-b' },
+  {
+    label:
+      'Špatně označené, nesprávně přiřazené nebo nesprávně identifikované informace',
+    value: 'distortion-c',
+  },
+  { label: 'Zavádějící informace', value: 'distortion-d' },
+  { label: 'Nadhodnocené / podhodnocené', value: 'distortion-e' },
+  { label: 'Zaměněné nebo smíchané informace', value: 'distortion-f' },
+  { label: 'Upravený obsah', value: 'distortion-g' },
+  { label: 'Nahraný (inscenovaný) obsah', value: 'distortion-h' },
+  { label: 'Přetvořený obsah', value: 'distortion-i' },
+  { label: 'Vykonstruované informace', value: 'distortion-j' },
+  { label: 'Podvodný obsah', value: 'distortion-k' },
+  { label: 'Koordinované neautentické chování', value: 'distortion-l' },
+  { label: 'Pravdivé', value: 'distortion-m' },
 ]
 
-export const formatTypes = [
+export const formatType = [
+  { label: 'Obrázek', value: 'image' },
   { label: 'Video', value: 'video' },
   { label: 'Text', value: 'text' },
-  { label: 'Obrázek', value: 'picture' },
+  { label: 'Zvuk', value: 'audio' },
+  { label: 'Jiné', value: 'other' },
 ]
 
 // Create a type for the topic IDs
@@ -39,35 +103,41 @@ export type Topic = typeof topic
 export type TopicKey = keyof Topic
 
 // Create a type for the subtopic IDs
-type SubtopicId = (typeof topic)[TopicId]['subtopic'][number]['id']
+type SubtopicId = (typeof topic)[TopicId]['subtopics'][number]['id']
 
 // Create the form schema
 export const euroclimateFormSchema = z
   .object({
-    topic: z.enum(['Extreme weather events', 'Transport'], {
+    topic: z.enum(Object.keys(topic) as [string, ...string[]], {
       required_error: 'Zvolte téma',
     }),
-    subtopic: z.array(z.string()).min(1, 'Zvolte alespoň jedno podtéma'),
+    subtopics: z.array(z.string()).min(1, 'Zvolte alespoň jedno podtéma'),
     distortionType: z
       .array(z.string())
       .min(1, 'Vyberte alespoň jeden typ dezinformace'),
-    appearanceUrl: z.string().min(1, 'Zadejte URL'),
-    appearanceDate: z.string(),
-    archiveUrl: z.string().optional(),
-    format: z.enum(['video', 'text', 'picture'], {
-      required_error: 'Vyberte formát',
-    }),
+    appearances: z
+      .array(
+        z.object({
+          appearanceUrl: z.string().min(1, 'Zadejte URL'),
+          appearanceDate: z.string(),
+          archiveUrl: z.string().optional(),
+          format: z.enum(['image', 'video', 'text', 'audio', 'other'], {
+            required_error: 'Vyberte formát',
+          }),
+        })
+      )
+      .min(1, 'Musíte přidat alespoň jeden výskyt'),
   })
   .refine(
     (data) => {
       // Validate that the selected subtopic belongs to the selected topic
       const selectedTopic = topic[data.topic as TopicId]
-      return data.subtopic.every((subtopicId) =>
-        selectedTopic.subtopic.some((subtopic) => subtopic.id === subtopicId)
+      return data.subtopics.every((subtopicId) =>
+        selectedTopic.subtopics.some((subtopic) => subtopic.id === subtopicId)
       )
     },
     {
       message: 'Invalid subtopic for selected topic',
-      path: ['subtopic'],
+      path: ['subtopics'],
     }
   )
