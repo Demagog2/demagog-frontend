0957ca23394a43a84815c7e97515c56dadab6d42
Zuzana Belaskova <z.belaskova@gmail.com>
2025-05-29 11:55:21 +0200

    Removed unused imports

--- components/admin/articles/AdminArticleIllustrationDialog.tsx
+++ components/admin/articles/AdminArticleIllustrationDialog.tsx
@@ -4,7 +4,6 @@ import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import {
-  Button,
   Dialog,
   DialogBackdrop,
   DialogPanel,
