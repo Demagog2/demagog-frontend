60ff968e663e0c4d4a1ebf0789088c63ade9e2c8
Zuzana Belaskova <z.belaskova@gmail.com>
2025-06-05 09:25:37 +0200

    Integration button displays only for facebook factcheck in article edit

--- components/admin/articles/AdminArticleForm.tsx
+++ components/admin/articles/AdminArticleForm.tsx
@@ -695,18 +695,20 @@ export function AdminArticleForm(props: {
                 )}
               />
             </Field>
-            <span className="hidden sm:block w-full sm:w-fit lg:w-full">
-              <a
-                href={`/beta/admin/articles/${article?.id}/integrations`}
-                className="inline-flex items-center w-full justify-center rounded-md bg-white px-6 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-              >
-                <GlobeAltIcon
-                  aria-hidden="true"
-                  className="-ml-0.5 mr-1.5 h-5 w-5 text-gray-400"
-                />
-                Integrace
-              </a>
-            </span>
+            {article?.articleType === 'facebook_factcheck' && (
+              <span className="block w-fit lg:w-full">
+                <a
+                  href={`/beta/admin/articles/${article?.id}/integrations`}
+                  className="inline-flex items-center w-full justify-center rounded-md bg-white px-6 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                >
+                  <GlobeAltIcon
+                    aria-hidden="true"
+                    className="-ml-0.5 mr-1.5 h-5 w-5 text-gray-400"
+                  />
+                  Integrace
+                </a>
+              </span>
+            )}
           </Fieldset>
         </AdminFormSidebar>
       </AdminFormContent>
