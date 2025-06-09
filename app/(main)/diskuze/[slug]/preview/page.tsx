1e7fbf839e4eba2e6336881f121f461289a6bed2
Zuzana Belaskova <z.belaskova@gmail.com>
2025-06-09 15:49:09 +0200

    Created preview page route

---
+++ app/(main)/diskuze/[slug]/preview/page.tsx
@@ -0,0 +1,192 @@
+import { query } from '@/libs/apollo-client'
+import { gql } from '@/__generated__'
+import { TypedDocumentNode } from '@graphql-typed-document-node/core'
+import { ArticlePlayer } from '@/components/article/player/ArticlePlayer'
+import { ArticleIllustration } from '@/components/article/ArticleIllustration'
+import { ArticleSegments } from '@/components/article/ArticleSegments'
+import { FacebookFactcheckMetadata } from '@/components/article/metadata/FacebookFactcheckArticleMetadata'
+import { DebateArticleMetadata } from '@/components/article/metadata/DebateArticleMetadata'
+import { StaticArticleMetadata } from '@/components/article/metadata/StaticArticleMetadata'
+import { Metadata } from 'next'
+import { permanentRedirect } from 'next/navigation'
+import {
+  getCanonicalMetadata,
+  getMetadataTitle,
+  getRobotsMetadata,
+} from '@/libs/metadata'
+import { Iframely } from '@/components/site/Iframely'
+import { DefaultMetadata } from '@/libs/constants/metadata'
+import { truncate } from 'lodash'
+import { imagePath } from '@/libs/images/path'
+import { notFound } from 'next/navigation'
+import Script from 'next/script'
+import { ArticleSocialShareButtons } from '@/components/article/ArticleSocialShareButtons'
+
+export const revalidate = 180
+export const dynamic = 'force-static'
+
+export async function generateStaticParams() {
+  const { data } = await query({
+    query: gql(`
+      query articleGenerateStaticParams($first: Int) {
+        homepageArticlesV3(first: $first) {
+          nodes {
+            ... on Article {
+              slug
+            }
+            ... on SingleStatementArticle {
+              slug
+            }
+          }
+        }
+      }
+    `),
+    variables: {
+      first: 20,
+    },
+  })
+
+  return (
+    data.homepageArticlesV3?.nodes?.flatMap((node) =>
+      node ? [{ slug: node.slug }] : []
+    ) ?? []
+  )
+}
+
+export async function generateMetadata(props: {
+  params: { slug: string }
+}): Promise<Metadata> {
+  const {
+    data: { articleV2: article },
+  } = await query({
+    query: gql(`
+      query ArticleMetadata($id: ID!) {
+        articleV2(id: $id) {
+          slug
+          title
+          perex
+          illustration(size: medium)
+        }
+      }
+    `),
+    variables: {
+      id: props.params.slug,
+    },
+  })
+
+  if (!article) {
+    notFound()
+  }
+
+  const title = getMetadataTitle(article.title)
+  const description = truncate(article.perex ?? '', { length: 230 })
+  const images = article.illustration
+    ? { images: imagePath(article.illustration) }
+    : {}
+  const url = `${DefaultMetadata.openGraph?.url}/diskuze/${article.slug}`
+
+  return {
+    title,
+    description,
+    openGraph: {
+      ...DefaultMetadata.openGraph,
+      ...images,
+      url,
+      title,
+      description,
+    },
+    twitter: {
+      ...DefaultMetadata.twitter,
+      ...images,
+      title,
+      description,
+      card: 'summary_large_image',
+    },
+    ...getRobotsMetadata(),
+    ...getCanonicalMetadata(`/diskuze/${article.slug}`),
+  }
+}
+
+export default async function Article(props: { params: { slug: string } }) {
+  const { slug } = props.params
+
+  const {
+    data: { articleV3: article },
+  } = await query({
+    query: gql(`
+      query ArticlePreviewDetail($slug: ID!) {
+        articleV3(id: $slug) {
+          ... on Article {
+            title
+            articleType
+            perex
+            showPlayer
+            ...ArticleSocialShareButtons
+            ...DebateAticleMetadata
+            ...FacebookFactcheckMetadata
+            ...StaticArticleMetadata
+            ...ArticleSegments
+            ...ArticlePlayer
+            ...ArticleIllustration
+          }
+          ... on SingleStatementArticle {
+            statement {
+              id
+            }
+          }
+        }
+      }
+    `),
+    variables: {
+      slug: slug,
+    },
+  })
+
+  if (!article) {
+    notFound()
+  }
+
+  if (article.__typename === 'SingleStatementArticle') {
+    permanentRedirect(`/statements/${article.statement?.id}`)
+  }
+
+  if (article.__typename !== 'Article') {
+    return null
+  }
+
+  return (
+    <div className="container px-3 article-redesign text-align-start col-sm-8 mx-sm-auto">
+      <div>
+        <div>
+          <div>
+            <h1 className="display-1 fw-bold px-3 px-sm-0">{article.title}</h1>
+            <div className="d-flex justify-content-end">
+              <ArticleSocialShareButtons article={article} />
+            </div>
+
+            {article.showPlayer ? (
+              <ArticlePlayer article={article} />
+            ) : (
+              <ArticleIllustration article={article} />
+            )}
+            <div className="mt-4 mt-md-9">
+              <span className="perex">{article.perex}</span>
+            </div>
+          </div>
+          <DebateArticleMetadata article={article} />
+          <FacebookFactcheckMetadata article={article} />
+          <StaticArticleMetadata article={article} />
+        </div>
+        <div>
+          <ArticleSegments data={article} />
+        </div>
+      </div>
+      <Iframely />
+      <Script
+        src="https://platform.twitter.com/widgets.js"
+        strategy="lazyOnload"
+        crossOrigin="anonymous"
+      />
+    </div>
+  )
+}
