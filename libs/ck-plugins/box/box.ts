2e1c285b4f8cd3e267108bcd87405ee57df24302
Vaclav Bohac <bohac.v@gmail.com>
2025-07-02 08:31:32 +0200

    Bump ckeditor to latest version

--- libs/ck-plugins/box/box.ts
+++ libs/ck-plugins/box/box.ts
@@ -1,5 +1,10 @@
 import classNames from 'classnames'
-import { ButtonView, Editor, icons } from 'ckeditor5'
+import { ButtonView, Editor } from 'ckeditor5'
+import {
+  IconObjectFullWidth,
+  IconObjectRight,
+  IconColorPalette,
+} from '@ckeditor/ckeditor5-icons'
 import { BoxCommand } from './box-command'
 import { BoxToggleBgCommand } from './box-toggle-bg-command'
 import { BoxToggleFloatCommand } from './box-toggle-float-command'
@@ -117,7 +122,7 @@ export function Box(editor: Editor) {
 
     view.set({
       label: editor.locale.t('Rámeček'),
-      icon: icons.objectFullWidth,
+      icon: IconObjectFullWidth,
       isToggleable: true,
       tooltip: true,
     })
@@ -132,7 +137,7 @@ export function Box(editor: Editor) {
 
     view.set({
       label: editor.locale.t('Obtékání'),
-      icon: icons.objectBlockRight,
+      icon: IconObjectRight,
       isToggleable: true,
       tooltip: true,
     })
@@ -147,7 +152,7 @@ export function Box(editor: Editor) {
 
     view.set({
       label: editor.locale.t('Pozadí'),
-      icon: icons.colorPalette,
+      icon: IconColorPalette,
       isToggleable: true,
       tooltip: true,
     })
