67af94f93fc004d8c68a0807c1ffa1e08129cca5
Zuzana Belaskova <z.belaskova@gmail.com>
2025-07-02 12:46:05 +0200

    Removed unnecessary import

--- components/modals/DonateModal.tsx
+++ components/modals/DonateModal.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useCallback, useEffect, useState } from 'react'
+import { useCallback, useEffect } from 'react'
 import classNames from 'classnames'
 import DonateWidget from '@/components/site/DonateWidget'
 
