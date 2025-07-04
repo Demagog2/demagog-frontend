1ca51bd954b280bb00cd3a03426cb94867ac6fb3
Zuzana Belaskova <z.belaskova@gmail.com>
2025-07-04 15:40:34 +0200

    Present users visible

--- components/admin/sources/statements/AdminAssessmentForm.tsx
+++ components/admin/sources/statements/AdminAssessmentForm.tsx
@@ -54,6 +54,8 @@ import {
   useStatementSubscription,
 } from '@/libs/web-sockets/ActionCableProvider'
 import { AdminUserAvatarPure } from '@/components/admin/users/AdminUserAvatar'
+import { AdminPresentUsers } from './AdminPresentUsers'
+import { PresentUser } from './AdminPresentUsers'
 
 const RichTextEditor = dynamic(
   () => import('@/components/admin/forms/RichTextEditor'),
@@ -221,9 +223,7 @@ function AdminAssessmentForm(props: {
     props.statement
   )
 
-  const [presentUsers, setPresentUsers] = useState<
-    { id: number; fullName: string }[]
-  >([])
+  const [presentUsers, setPresentUsers] = useState<PresentUser[]>([])
 
   const onPresenceUpdate = useCallback((message: PresenceUpdated) => {
     setPresentUsers(
@@ -952,6 +952,10 @@ function AdminAssessmentForm(props: {
                 <AdminUserAvatarPure key={user.id} user={user} size="small" />
               ))}
             </div>
+          )}
+
+          {props.activeUsersEnabled && (
+            <AdminPresentUsers presentUsers={presentUsers} />
           )}
 
           <AdminStatementActivities
--- components/admin/sources/statements/AdminPresentUsers.tsx
+++ components/admin/sources/statements/AdminPresentUsers.tsx
@@ -1,8 +1,63 @@
-// TODO: Add fields of present users, id, display name and avatar (avatar is optional)
+'use client'
 
-export type PresentUser = {}
+import { useState } from 'react'
+import { AdminUserAvatarPure } from '../../users/AdminUserAvatar'
+
+export type PresentUser = {
+  id: number
+  fullName: string
+  avatar?: string | null
+}
 
 export function AdminPresentUsers(props: { presentUsers: PresentUser[] }) {
-  // TODO: Add content of present users from AdminAssessmentForm
-  return null
+  const { presentUsers } = props
+  const maxVisibleUsers = 10
+  const hasMoreUsers = presentUsers.length > maxVisibleUsers
+  const visibleUsers = presentUsers.slice(0, maxVisibleUsers)
+  const invisibleUsers = presentUsers.slice(maxVisibleUsers)
+
+  const [showMoreUsers, setShowMoreUsers] = useState(false)
+
+  return (
+    <div className="mb-4">
+      <div className="mb-3">
+        <h3 className="text-sm font-semibold text-gray-900">
+          Aktivní uživatelé:
+        </h3>
+      </div>
+      {presentUsers.length === 0 ? (
+        <div className="text-sm text-gray-500">
+          <span>Žádní aktivní uživatelé</span>
+        </div>
+      ) : (
+        <>
+          <div className="flex -space-x-0.5">
+            {visibleUsers.map((user) => (
+              <AdminUserAvatarPure key={user.id} user={user} size="small" />
+            ))}
+            {hasMoreUsers && (
+              <div className="relative">
+                <span
+                  className="inline-flex items-center justify-center rounded-full bg-gray-200 h-6 w-6 ring-2 ring-white cursor-default"
+                  onMouseEnter={() => setShowMoreUsers(true)}
+                  onMouseLeave={() => setShowMoreUsers(false)}
+                >
+                  <span className="text-sm font-medium leading-none text-gray-600">
+                    +{presentUsers.length - maxVisibleUsers}
+                  </span>
+                </span>
+                {showMoreUsers && (
+                  <div className="absolute right-full right-1 mt-1 px-2 py-1 rounded-lg bg-gray-200 w-max">
+                    {invisibleUsers.map((user) => (
+                      <p>{user.fullName}</p>
+                    ))}
+                  </div>
+                )}
+              </div>
+            )}
+          </div>
+        </>
+      )}
+    </div>
+  )
 }
